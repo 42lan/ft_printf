f3fbc893e0f6f983728fe0dcecec1d497de31f0c
4slan <amalsago@student.42.fr>
2019-02-06 02:22:14 +0100

    retured variable renamed into nopc

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,19 +6,24 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 23:43:59 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/06 00:10:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/06 00:21:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+/*
+** nopc - number of printed characers
+** ap	- argument parameter
+*/
+
 int				ft_printf(const char *restrict format, ...)
 {
-	int			returned;
+	int			nopc;
 	va_list		ap;
 
 	va_start(ap, format);
-	returned = parsing(ap, format);
+	nopc = parsing(ap, format);
 	va_end(ap);
-	return (returned);
+	return (nopc);
 }
