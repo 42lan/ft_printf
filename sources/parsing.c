d08b33a6169909aec063787a9dc3277e63432f05
4slan <amalsago@student.42.fr>
2019-02-06 23:27:54 +0100

    format variable renamed into str

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,40 +6,40 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 00:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/06 02:36:54 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/06 22:42:05 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int			parsing(va_list ap, const char *restrict format)
+int			parsing(va_list ap, const char *restrict str)
 {
 	(void)ap;
 	char	*s;
 	char	c;
-	char	d;
+	int		d;
 
-	while (*format)
+	while (*str)
 	{
-		if (*format == '%' && (*(format + 1) != '%' && *(format - 1) != '%'))
+		if (*str == '%' && (*(str + 1) != '%' && *(str - 1) != '%'))
 		{
-			if (*(format + 1) == 's')
+			if (*(str + 1) == 's')
 			{
 				s = va_arg(ap, char *);
 				ft_putstr(s);
 			}
-			if (*(format + 1) == 'c')
+			if (*(str + 1) == 'c')
 			{
 				c = va_arg(ap, int);
 				ft_putchar(c);
 			}
-			if (*(format + 1) == 'd')
+			if (*(str + 1) == 'd')
 			{
 				d = va_arg(ap, int);
 				ft_putnbr(d);
 			}
 		}
-		format++;
+		str++;
 	}
 	return (1);
 }
