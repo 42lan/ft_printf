8796986e921cb27f178aeb310d8ea8f2f18607a2
4slan <amalsago@student.42.fr>
2019-02-06 09:12:25 +0100

    percent variable removed; defining s, c and d to use with va_arg

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 00:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/06 02:10:23 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/06 02:36:54 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,31 @@
 int			parsing(va_list ap, const char *restrict format)
 {
 	(void)ap;
-	int		percent;
+	char	*s;
+	char	c;
+	char	d;
 
-	percent = 0;
 	while (*format)
 	{
 		if (*format == '%' && (*(format + 1) != '%' && *(format - 1) != '%'))
 		{
 			if (*(format + 1) == 's')
+			{
+				s = va_arg(ap, char *);
+				ft_putstr(s);
+			}
+			if (*(format + 1) == 'c')
+			{
+				c = va_arg(ap, int);
+				ft_putchar(c);
+			}
+			if (*(format + 1) == 'd')
+			{
+				d = va_arg(ap, int);
+				ft_putnbr(d);
+			}
 		}
-			percent++;
-		printf("+");
 		format++;
 	}
-	return (percent);
+	return (1);
 }
