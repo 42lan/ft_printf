d974181852485bb8336baebc6fcd16a5b64d5b65
4slan <amalsago@student.42.fr>
2019-02-06 02:23:45 +0100

    debut of implementing parsing

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,15 +6,27 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 00:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/06 00:10:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/06 02:10:23 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		parsing(va_list ap, const char *restrict format)
+int			parsing(va_list ap, const char *restrict format)
 {
 	(void)ap;
-	(void)format;
-	return (1);
+	int		percent;
+
+	percent = 0;
+	while (*format)
+	{
+		if (*format == '%' && (*(format + 1) != '%' && *(format - 1) != '%'))
+		{
+			if (*(format + 1) == 's')
+		}
+			percent++;
+		printf("+");
+		format++;
+	}
+	return (percent);
 }
