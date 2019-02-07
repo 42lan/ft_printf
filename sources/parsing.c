1adb6fd39733e032bca1df8710d2a3411fb2256d
4slan <amalsago@student.42.fr>
2019-02-07 05:40:21 +0100

    ap renamed into arg. added new condition if is not percent sign

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,21 +6,23 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 00:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/06 22:42:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/07 04:47:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int			parsing(va_list ap, const char *restrict str)
+int			parsing(va_list arg, const char *restrict str)
 {
 	(void)ap;
 	char	*s;
-	char	c;
+	int		c;
 	int		d;
 
 	while (*str)
 	{
+		if (*str != '%')
+			ft_putchar(*str);
 		if (*str == '%' && (*(str + 1) != '%' && *(str - 1) != '%'))
 		{
 			if (*(str + 1) == 's')
