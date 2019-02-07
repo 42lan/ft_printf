e0bbd227fb7cb6adfc4dfa8ddf478ba19573d53d
4slan <amalsago@student.42.fr>
2019-02-07 23:13:26 +0100

    adding ifndef and normalize following directives

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,15 +6,20 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 23:37:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/07 04:48:02 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/07 23:07:13 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
 /* ########################################################################## */
-#include <stdio.h> /* ####################################################### */
+# include <stdio.h> /* ###################################################### */
 /* ########################################################################## */
-#include <stdarg.h>
-#include "../libft/libft.h"
+# include <stdarg.h>
+# include "../libft/libft.h"
 
 int		ft_printf(const char *restrict format, ...);
 int		parsing(va_list arg, const char *restrict str);
+
+#endif
