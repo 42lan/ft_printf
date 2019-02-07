983a69fb7a48096f798c336091832c809da68a28
4slan <amalsago@student.42.fr>
2019-02-08 00:58:15 +0100

    adding parameter variagble to s_spec, changing types for precision and length, creating s_flags

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,25 +6,35 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 23:37:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/08 00:04:30 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/08 00:40:31 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-/* ########################################################################## */
-# include <stdio.h> /* ###################################################### */
-/* ########################################################################## */
+/* -------------------------------------------------------------------------- */
+# include <stdio.h> /* ------------------------------------------------------ */
+/* -------------------------------------------------------------------------- */
 # include <stdarg.h>
 # include "../libft/libft.h"
 
+typedef struct	s_flags
+{
+	unsigned	minus	: 1;
+	unsigned	plus	: 1;
+	unsigned	space	: 1;
+	unsigned	zero	: 1;
+	unsigned	hash	: 1;
+}				t_flags;
+
 typedef struct	s_spec
 {
-	char		*flags;
+	int			parameter;
+	t_flags		*flag;
 	int			width;
-	char		*precision;
-	int			length;
+	int			precision;
+	char		*length;
 }				t_spec;
 
 typedef struct	s_type
