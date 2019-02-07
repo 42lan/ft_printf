9feeafde86cfd4afb88152bb0765f226da28b795
4slan <amalsago@student.42.fr>
2019-02-07 04:46:19 +0100

    ap and nopc renamed into arg and done; comments removed

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,24 +6,19 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 23:43:59 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/06 00:21:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/07 04:45:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/*
-** nopc - number of printed characers
-** ap	- argument parameter
-*/
-
 int				ft_printf(const char *restrict format, ...)
 {
-	int			nopc;
-	va_list		ap;
+	va_list		arg;
+	int			done;
 
-	va_start(ap, format);
-	nopc = parsing(ap, format);
-	va_end(ap);
-	return (nopc);
+	va_start(arg, format);
+	done = parsing(arg, format);
+	va_end(arg);
+	return (done);
 }
