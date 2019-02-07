fc26b6751b15b2c9aa2371c1000e1a8040e4b916
4slan <amalsago@student.42.fr>
2019-02-07 23:13:48 +0100

    ap renamed in arg

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 00:02:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/07 04:47:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/07 23:08:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,28 @@
 
 int			parsing(va_list arg, const char *restrict str)
 {
-	(void)ap;
+	(void)arg;
 	char	*s;
 	int		c;
 	int		d;
 
 	while (*str)
 	{
-		if (*str != '%')
-			ft_putchar(*str);
 		if (*str == '%' && (*(str + 1) != '%' && *(str - 1) != '%'))
 		{
 			if (*(str + 1) == 's')
 			{
-				s = va_arg(ap, char *);
+				s = va_arg(arg, char *);
 				ft_putstr(s);
 			}
 			if (*(str + 1) == 'c')
 			{
-				c = va_arg(ap, int);
+				c = va_arg(arg, int);
 				ft_putchar(c);
 			}
 			if (*(str + 1) == 'd')
 			{
-				d = va_arg(ap, int);
+				d = va_arg(arg, int);
 				ft_putnbr(d);
 			}
 		}
