8cfd5b9c34615d8335d39f2b9a1e5a5da0cce125
4slan <amalsago@student.42.fr>
2019-02-08 03:46:56 +0100

    chechking if ft_printf returns a negative value

--- sources/main.c
+++ sources/main.c
@@ -19,15 +19,9 @@ int			main(int ac, char **av)
 	char	*world = "World";
 	char	exclam = '!';
 	int		nb = 42;
-	t_spec	*x;
-	
-	if (!(x = (t_spec *)malloc(sizeof(t_spec))))
-		return (1);
-	if (!(x->flags = (t_flags *)malloc(sizeof(t_flags))))
-		return (1);
-	x->flags->minus = 1;
-	printf("%lu\n\n", sizeof(x->flags));
+
 	printf("Hello, %s%c Ingore following % d%%\n", world, exclam, nb);
-	ft_printf("Hello, %s%c Ingore following % d%%\n", world, exclam, nb);
+	if (ft_printf("Hello, %s%c Ingore following % d%%\n", world, exclam, nb) < 0)
+		return (1);
 	return (0);
 }
