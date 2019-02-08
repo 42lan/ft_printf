1c6c71ab6a4473f4405018a1707c1d31dff46715
4slan <amalsago@student.42.fr>
2019-02-08 01:01:11 +0100

    declaring t_spec *x to test structure with bit field

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/07 04:08:54 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/08 01:00:38 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,14 @@ int			main(int ac, char **av)
 	char	*world = "World";
 	char	exclam = '!';
 	int		nb = 42;
-
+	t_spec	*x;
+	
+	if (!(x = (t_spec *)malloc(sizeof(t_spec))))
+		return (1);
+	if (!(x->flag = (t_flags *)malloc(sizeof(t_flags))))
+		return (1);
+	x->flag->minus = 1;
+	printf("%d\n\n", x->flag->minus);
 	printf("Hello, %s%c Ingore following % d%%\n", world, exclam, nb);
 	ft_printf("Hello, %s%c Ingore following % d%%\n", world, exclam, nb);
 	return (0);
