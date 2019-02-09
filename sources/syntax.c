406dd25021e55434b68e9942c1c97440d826b872
4slan <amalsago@student.42.fr>
2019-02-09 02:53:04 +0100

    adding check of specs in comment; reset_flags() removed from check_flags()

--- sources/syntax.c
+++ sources/syntax.c
@@ -12,22 +12,26 @@
 
 #include "ft_printf.h"
 
-int			check_syntax(const char *str, t_type *type)
+int			check_syntax(const char c, t_type *type)
 {
 	/*	%[parameter][flags][width][.precision][length]type	*/
 	
-	check_flags(str, type->specifiers->flags);
-	check_type(str, type);
+	//check_parameter();
+	check_flags(c, type->specifiers->flags);
+	//check_width();
+	//check_precision();
+	//check_length();
+	//check_type(str, type);
 	return (1);
 }
 
 void		reset_flags(t_flags *flags)
 {
-	flags->minus = 0;
-	flags->plus = 0;
-	flags->space = 0;
-	flags->zero = 0;
-	flags->hash = 0;
+	flags->minus	= 0;
+	flags->plus		= 0;
+	flags->space	= 0;
+	flags->zero		= 0;
+	flags->hash		= 0;
 }
 
 int			isflag(const char c)
@@ -35,35 +39,49 @@ int			isflag(const char c)
 	return (c == '-' || c == '+' || c == ' ' || c == '0' || c == '#');
 }
 
-int			check_flags(const char *str, t_flags *flags)
+int			check_flags(const char c, t_flags *flags)
 {
-	reset_flags(flags);
 	//	printf("%-#10x \n", number)		Correct
 	//	printf("%-*10x \n", number)		Incorrect
 	//	"+f \n"							Correct TEST
-	while (isflag(*str))
+	while (isflag(c))
 	{
-		if (*str == '-')
+		if (c == '-')
+		{
 			flags->minus = 1;
-		else if (*str == '+')
+			return (1);		
+		}
+		else if (c == '+')
+		{
 			flags->plus = 1;
-		else if (*str == ' ')
+			return (1);		
+		}
+		else if (c == ' ')
+		{
 			flags->space = 1;
-		else if (*str == '0')
+			return (1);		
+		}
+		else if (c == '0')
+		{
 			flags->zero = 1;
-		else if (*str == '#')
+			return (1);		
+		}
+		else if (c == '#')
+		{
 			flags->hash = 1;
+			return (1);		
+		}
 		else
 		{
 			reset_flags(flags);
 			return (0);
-		}		
-		str++;
+		}
 	}
-	return (1);
+	return (0);
 }
 
 int			check_type(const char *str, t_type *type)
 {
-	if (*str)
+	(void)str; (void)type;
+	return (1);
 }
