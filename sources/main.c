1380bd670a8c4f3973922fead614a9912968dabb
4slan <amalsago@student.42.fr>
2019-02-13 23:48:40 +0100

    date update

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/11 03:52:34 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/13 23:45:16 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int			main(int ac, char **av)
 	//printf("%%_%+f ", nb);
 	if (ft_printf("%%_%0+.f= %d", nb, nb/2) < 0)
 		return (1);
+	// %_
 	/*
 	printf("Hello, %s%c %+.2f%%\n", world, exclam, nb);
 	if (ft_printf("Hello, %s%c %+.2f%%\n", world, exclam, nb) < 0)
