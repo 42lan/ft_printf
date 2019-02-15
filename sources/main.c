67aac83b64de9327c18532d37ad0b3e70629530f
4slan <amalsago@student.42.fr>
2019-02-15 10:34:02 +0100

    all function calls are commented except ft_printf()

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/14 04:54:28 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/15 09:26:24 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int			main(void)
 	int		neg = -42;
 	float	pi = 3.141592;
 
-	printf("----------printf----------\n");
-	printf("Hello, %.5s%c\n[% 4d%% ] Loading...\nPi number %+3f\nFahrenheit %d\n%dºC\n", world, c, pos, pi, fah, neg);
-	printf("----------ft_printf-------\n");
+	//printf("----------printf----------\n");
+	//printf("Hello, %.5s%c\n[% 4d%% ] Loading...\nPi number %+3f\nFahrenheit %d\n%dºC\n", world, c, pos, pi, fah, neg);
+	//printf("----------ft_printf-------\n");
 	ft_printf("Hello, %.5s%c\n[% 4d%% ] Loading...\nPi number %+3f\nFahrenheit %d\n%dºC\n", world, c, pos, pi, fah, neg);
 	return (0);
 }
