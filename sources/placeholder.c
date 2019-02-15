d819adc902b4f6c3d30b6bcb5e3eaefffe5cc0b7
4slan <amalsago@student.42.fr>
2019-02-15 10:34:20 +0100

    is_placeholder() defined

--- sources/placeholder.c
+++ sources/placeholder.c
@@ -6,8 +6,22 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:50:25 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/15 05:50:56 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/15 09:26:27 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+int		is_placeholder(const char **format)
+{
+	while (**format)
+	{
+		if (is_conversion_type(**format))
+		{
+			printf("%c", **format);
+			return (1);
+		}
+		(*format)++;
+	}
+	return (1);
+}
