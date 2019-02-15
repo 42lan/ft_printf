8e584908093f73d8098376a8b034a408f4f3a89e
4slan <amalsago@student.42.fr>
2019-02-15 10:33:06 +0100

    some part are commented

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/14 06:03:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/15 09:26:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,16 @@ int				ft_printf(const char *format, ...)
 	va_start(arg, format);
 	while (*format)
 	{
-		if (buffer.index <= BUFF_SIZE - 1)
-			print_buffer(&buffer);
-		if (*format != '%')
-			buffer.string[buffer.index++] = *format;
+		//if (buffer.index == BUFF_SIZE - 1) // last line isn't print because < BUFF_SIZE
+		//	print_buffer(&buffer);
+		if (*format == '%' && is_placeholder(&format))
+		{
+			//ft_putendl(format);
+		}
 		else
 		{
 			buffer.string[buffer.index++] = *format;
+			//buffer.string[buffer.index++] = *format;
 		}
 		format++;
 	}
