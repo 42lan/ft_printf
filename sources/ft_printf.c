d855875946f05000c64b4a71e18af65410dd1f87
4slan <amalsago@student.42.fr>
2019-02-16 05:43:40 +0100

    declaring pointer to placeholder; avoid "%%" condition; calling is_placeholder()

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,28 +6,33 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/15 09:26:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/16 05:39:24 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int				ft_printf(const char *format, ...)
+int					ft_printf(const char *format, ...)
 {
-	int			done;
-	va_list		arg;
-	t_buffer	buffer;
+	int				done;
+	va_list			arg;
+	t_buffer		buffer;
+	t_placeholder	*placeholder;
 
 	done = 0;
 	buffer = initialize_buffer();
+	placeholder = initialize_placeholder();
 	va_start(arg, format);
 	while (*format)
 	{
 		//if (buffer.index == BUFF_SIZE - 1) // last line isn't print because < BUFF_SIZE
 		//	print_buffer(&buffer);
-		if (*format == '%' && is_placeholder(&format))
+		if (*format == '%' && *(format + 1) == '%')
+			format = format + 2;
+		if (*format == '%' && is_placeholder(&format, placeholder))
 		{
 			//ft_putendl(format);
+			//return (1);
 		}
 		else
 		{
