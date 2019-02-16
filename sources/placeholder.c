a7e85307112c8079df636efb1cff723ab44bddde
4slan <amalsago@student.42.fr>
2019-02-16 09:17:10 +0100

    modifying completely is_placeholder() to return just 0 or 1; placing print_placeholder() here

--- sources/placeholder.c
+++ sources/placeholder.c
@@ -6,16 +6,30 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:50:25 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/16 05:39:29 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/16 09:09:18 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		is_placeholder(const char **format, t_placeholder *placeholder)
+int		is_placeholder(const char **format)//, t_placeholder *placeholder)
 {
+	//placeholder = initialize_placeholder();
 	if ((**format + 1) == '%')
 		return (0);
+	while (**format)
+	{
+		if (is_conversion_type(**format))
+		{
+			//ft_putchar(**format);
+			//ft_putchar('\n');
+			return (1);
+		}
+		(*format)++;
+	}
+	//ft_putchar(**format);
+	//ft_putchar('\n');
+	/*
 	ft_putchar(**format);
 	(*format)++;
 	while (!is_conversion_type(**format))
@@ -26,5 +40,27 @@ int		is_placeholder(const char **format, t_placeholder *placeholder)
 	}
 	ft_putchar(**format);
 	ft_putchar('\n');
-	return (1);
+	print_placeholder(placeholder);
+	ft_putchar('\n');
+	*/
+	return (0);
 }
+
+void				print_placeholder(t_placeholder *placeholder)
+{
+	ft_putstr("%[");
+	ft_putnbr(placeholder->specs->parameter);
+	ft_putstr("][");
+	ft_putnbr(placeholder->specs->flags->space);
+	ft_putnbr(placeholder->specs->flags->hash);
+	ft_putnbr(placeholder->specs->flags->plus);
+	ft_putnbr(placeholder->specs->flags->minus);
+	ft_putnbr(placeholder->specs->flags->zero);
+	ft_putstr("][");
+	ft_putnbr(placeholder->specs->width);
+	ft_putstr("].[");
+	ft_putnbr(placeholder->specs->precision);
+	ft_putstr("][");
+	printf("%s]%c\n", placeholder->specs->length, placeholder->type);
+}
+
