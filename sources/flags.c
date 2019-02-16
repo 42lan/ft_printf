385eead78f28434ed7ab3c3d0959d7aa0dab09d5
4slan <amalsago@student.42.fr>
2019-02-16 05:41:31 +0100

    fuction related with flags placed here with one exception initialize_specifiers()

--- sources/flags.c
+++ sources/flags.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:00:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/15 09:26:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/16 05:41:00 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,74 @@
 int		is_flag(const char c)
 {
 	return (c == ' ' || c == '#' || c == '+' || c == '-' || c == '0');
+}
+
+/*
+** Checking if passed character in argument is one of the flags and
+** setting up the corresponding bit to 1
+*/
+
+void		set_flag(const char c, t_flags *flags)
+{
+	if (c == ' ')
+		set_space(flags);
+	else if (c == '#')
+		set_hash(flags);
+	else if (c == '+')
+		set_plus(flags);
+	else if (c == '-')
+		set_minus(flags);
+	else if (c == '0')
+		set_zero(flags);
+}
+
+void		set_space(t_flags *flags)
+{
+	flags->space = 1;
+}
+
+void		set_hash(t_flags *flags)
+{
+	flags->hash = 1;
+}
+
+void		set_plus(t_flags *flags)
+{
+	flags->plus = 1;
+}
+
+void		set_minus(t_flags *flags)
+{
+	flags->minus = 1;
+}
+
+void		set_zero(t_flags *flags)
+{
+	flags->zero = 1;
+}
+
+
+/*
+** Initialization of type pointer to t_placeholder structure
+*/
+
+void	initialize_specifiers(t_spec *specifiers)
+{
+	specifiers->parameter = 0;
+	specifiers->width = 0;
+	specifiers->precision = 6;
+	specifiers->length = NULL;
+}
+
+/*
+** Setting up all fields of t_flags structure to 0
+*/
+
+void	initialize_flags(t_flags *flags)
+{
+	flags->space = 0;
+	flags->hash = 0;
+	flags->plus = 0;
+	flags->minus = 0;
+	flags->zero = 0;
 }
