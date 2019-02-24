86640f6158795eb6156c34927d2f0f75d99be240
4slan <amalsago@student.42.fr>
2019-02-24 13:16:31 +0100

    calling initialization(); removing comments

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/18 08:08:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/24 13:15:07 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,43 @@ int					ft_printf(const char *format, ...)
 	t_placeholder	*placeholder;
 
 	done = 0;
-	buffer = initialize_buffer();
-	placeholder = initialize_placeholder();	// Можно в check_syntax() обявить и инициализировать
+	initialization(&buffer, &placeholder);
 	va_start(arg, format);
 	while (*format)
 	{
-		if (buffer.index == BUFF_SIZE - 1) // Для того чтобы не было ошибки ->  zsh: abort      ./a.out
-			buffer = initialize_buffer();
+		//printf(">%c\n", *format);
 		/*
-		if (buffer.index == BUFF_SIZE - 1) // last line isn't print because < BUFF_SIZE. Need to be <= BUFF_SIZE?
-			print_buffer(&buffer);
+		if (buffer.index <= BUFF_SIZE - 1)
+		{
+			//print_buffer(&buffer);
+			buffer.index = 0;
+		}
 		*/
-		while (*format && *format == '%' && *(format + 1) == '%')
+		/*
+		while (*format == '%' && *(format + 1) == '%')
 		{
 			buffer.string[buffer.index++] = *format;
+			//printf("%c", *format);
 			format = format + 2;
+			done++;
 		}
-		if (*format == '%')	// Вот так уже лучше, но не красиво  |
+		*/
+		if (*format == '%')
 		{
 			//if (*format == '%' && is_placeholder(&format))
-			if (is_placeholder(format++))	// format++ для того чтобы пропустить % и начать анализ после этого знака
+			if (is_placeholder(format++))
 			{
 				check_syntax(format, placeholder);
 				print_placeholder(placeholder);
-				placeholder = initialize_placeholder();		// Могу ли я просто вызвать функцию без присвоения?
+				placeholder = initialize_placeholder();
 			}
 		}
 		else
+		{
 			buffer.string[buffer.index++] = *format;
+			//printf("%c", *format);
+			done++;
+		}
 		format++;
 	}
 	va_end(arg);
