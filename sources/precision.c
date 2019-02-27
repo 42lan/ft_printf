9050c18c235d8d4acb8afc28707f87952d60f3cb
4slan <amalsago@student.42.fr>
2019-02-27 17:04:12 +0100

    definition of precision()

--- sources/precision.c
+++ sources/precision.c
@@ -6,11 +6,23 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 15:18:09 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/27 15:18:17 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/27 16:58:29 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
 void		precision(const char **format, t_placeholder *placeholder)
 {
+	int			nb;
 
+	nb = 0;
+	(*format)++;
+	while (ft_isdigit((**format)))
+	{
+		nb *= 10;
+		nb = nb + (**format - '0');
+		(*format)++;
+	}
+	placeholder->specs->precision = nb;
 }
