4d71497083c7cf3e972a1c5bc2b77c3291285453
4slan <amalsago@student.42.fr>
2019-02-27 17:00:59 +0100

    using float with specifications

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/27 13:51:49 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/27 16:58:24 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int			main(void)
 {
 	//printf("\n%d\n", printf("Hello, %.5s%c", "World#$%^&*786ef", '!'));
 	//printf("\n%d\n", ft_printf("Hello, %-25%. %-10.5s%c", "World#$^&*786ef", '!'));
-	// printf("\n%d\n", printf("Hello, %010d", 42));
-	printf("\n%d\n", ft_printf("Hello, %010d", 42));
+	// printf("\n%d\n", printf("Hello, %010.8f", 42.1));
+	printf("\n%d\n", ft_printf("Hello, %010.8f", 42.1));
 	return (0);
 }
