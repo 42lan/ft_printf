d4be959569c6146faa86828ac8fb5150fcb0af04
4slan <amalsago@student.42.fr>
2019-02-27 13:54:10 +0100

    removing done variable and writing new else condition (to be instead while loop?

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/26 14:55:55 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/27 13:51:46 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,26 @@
 
 int				ft_printf(const char *format, ...)
 {
-	int			done;
 	va_list		ap;
 	t_buffer	buffer;
 
-	done = 0;
 	va_start(ap, format);
 	initialize_buffer(&buffer);
 	while (*format)
 	{
-		if (buffer.index == BUFF_SIZE - 1)
-		{
-			ft_putendl("YOP!");
-		}
 		while (*format && *format != '%')
 		{
 			buffer.string[buffer.index] = *format;
-			//ft_putchar(buffer.string[buffer.index]);
 			buffer.index++;
-			done++;
 			format++;
 		}
 		if (*format++ == '%')
-		{
 			parsing(&format, &ap, &buffer);
-			return 666;
-		}
+		/*
+		else
+			buffer.string[buffer.index++] = *format;
+		*/
 		format++;
 	}
-	return (done);
+	return (buffer.length);
 }
