3904e6ced1faf7f7900fafbf110437acb78e7360
4slan <amalsago@student.42.fr>
2019-02-27 13:54:40 +0100

    writing new printf

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/24 13:31:27 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/27 13:51:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int			main(void)
 {
 	//printf("\n%d\n", printf("Hello, %.5s%c", "World#$%^&*786ef", '!'));
-	printf("\n%d\n", ft_printf("Hello, %-10.5s%c", "World#$^&*786ef", '!'));
+	//printf("\n%d\n", ft_printf("Hello, %-25%. %-10.5s%c", "World#$^&*786ef", '!'));
+	//printf("\n%d\n", printf("Hello, % d", 42));
+	printf("\n%d\n", ft_printf("Hello, % s\n"));
 	return (0);
 }
