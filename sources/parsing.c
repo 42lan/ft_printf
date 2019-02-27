a613fcd44d662a71a266886e50292782934fe365
4slan <amalsago@student.42.fr>
2019-02-27 13:56:00 +0100

    commenting old manual parsing and definition of condition calling jump_table

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 09:25:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/26 15:22:31 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/27 13:51:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,48 @@ void				parsing(const char **format, va_list *ap, t_buffer *buffer)
 {
 	(void)ap;
 	(void)buffer;
-	int				nb;
+	/*
+	int				nb = 0;
+	*/
 	t_placeholder	*placeholder;
 
-	nb = 0;
 	placeholder = initialize_placeholder();
 	while (**format)
 	{
-		/* FLAGS */
+		if (ft_isprint(**format))
+			jump_table[**format - ' '](format, placeholder);
+		(*format)++;
+		/*
+		// FLAGS
 		if (is_flag(**format) && **format - 1 != '.')
 			set_flag(**format, placeholder->specs->flags);
-		/* WIDTH */
-		else if (is_flag(*(*format - 1)) && ft_isdigit(**format))
+		// WIDTH
+		else if (is_flag(*(*format - 1)) && (ft_isdigit(**format) || **format == '*'))
 		{
-			while (ft_isdigit(**format))
-			{
-				nb *= 10;
-				nb = nb + (**format - '0');
-				(*format)++;
-			}
-			placeholder->specs->width = nb;
-			nb = 0;
-			(*format)--;
-		}
-		/* PRECISION */
-		else if (**format == '.')
-		{
-			(*format)++;
 			if (**format == '*')
 			{
-				// take from arg list
-				ft_putendl("I'll take from arg list");
+				ft_putendl("I'll take WIDTH from argument list");
+				(*format)++;
+			}
+			else
+			{
+				while (ft_isdigit(**format))
+				{
+					nb *= 10;
+					nb = nb + (**format - '0');
+					(*format)++;
+				}
+				placeholder->specs->width = nb;
+				nb = 0;
+				(*format)--;
+			}
+		}
+		// PRECISION
+		else if (*(*format)++ == '.')
+		{
+			if (**format == '*')
+			{
+				ft_putendl("I'll take PRECISION from argument list");
 			}
 			else
 			{		
@@ -59,14 +70,21 @@ void				parsing(const char **format, va_list *ap, t_buffer *buffer)
 				}
 				placeholder->specs->precision = nb;
 				nb = 0;
+				(*format)--;
 			}
 		}
-		/* LENGTH */
+		// LENGTH
 		else if (ft_isdigit(*(*format - 1)))
 		{
 
 		}
+		// TYPE
+		else if (**format == '%')
+		{
+			ft_putendl("HOHO");
+			print_placeholder(placeholder);
+		}
 		(*format)++;
+		*/
 	}
-	print_placeholder(placeholder);
 }
