afa6db900ed322af4a6541d61671c0f3d5665ce8
4slan <amalsago@student.42.fr>
2019-02-28 10:39:10 +0100

    removing old commented code; adding check of placeholder->type is equal to 0

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 09:25:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/27 16:58:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/28 10:34:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,13 @@ void				parsing(const char **format, va_list *ap, t_buffer *buffer)
 {
 	(void)ap;
 	(void)buffer;
-	/*
-	int				nb = 0;
-	*/
 	t_placeholder	*placeholder;
 
 	placeholder = initialize_placeholder();
-	while (**format)
+	while (**format && !(placeholder->type))
 	{
 		if (ft_isprint(**format))
 			jump_table[**format - ' '](format, placeholder);
-		//(*format)++;
 	}
 	print_placeholder(placeholder);
 }
