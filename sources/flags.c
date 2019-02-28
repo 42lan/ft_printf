4376598ffc9a8816183cda7e95dfbc4358450f0a
4slan <amalsago@student.42.fr>
2019-02-28 14:53:39 +0100

    incrementing pointer of format string in all functions. Replacing unknown() to types.c

--- sources/flags.c
+++ sources/flags.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:00:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/28 10:34:11 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/28 14:46:06 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,39 +40,31 @@ void		set_flag(const char c, t_flags *flags)	// Может быть второй
 
 void			flag_space(const char **format, t_placeholder *placeholder)
 {
-	(void)format;
 	placeholder->specs->flags->space = 1;
+	(*format)++;
 }
 
 void			flag_hash(const char **format, t_placeholder *placeholder)
 {
-	(void)format;
 	placeholder->specs->flags->hash = 1;
+	(*format)++;
 }
 
 void			flag_plus(const char **format, t_placeholder *placeholder)
 {
-	(void)format;
 	placeholder->specs->flags->plus = 1;
 	placeholder->specs->flags->space = 0;
+	(*format)++;
 }
 
 void			flag_minus(const char **format, t_placeholder *placeholder)
 {
-	(void)format;
 	placeholder->specs->flags->minus = 1;
+	(*format)++;
 }
 
 void			flag_zero(const char **format, t_placeholder *placeholder)
 {
-	(void)format;
 	placeholder->specs->flags->zero = 1;
 	(*format)++;
-}
-
-void			unknown(const char **format, t_placeholder *placeholder)
-{
-	(void)format;
-	(void)placeholder;
-	ft_putendl("Sorry, I don't know it...");
 }
