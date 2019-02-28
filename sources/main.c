a02af60e1bf417c0df98c0e9bc21443ab270b208
4slan <amalsago@student.42.fr>
2019-02-28 14:56:11 +0100

    new format string

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/27 16:58:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/28 14:46:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int			main(void)
 {
 	//printf("\n%d\n", printf("Hello, %.5s%c", "World#$%^&*786ef", '!'));
 	//printf("\n%d\n", ft_printf("Hello, %-25%. %-10.5s%c", "World#$^&*786ef", '!'));
-	// printf("\n%d\n", printf("Hello, %010.8f", 42.1));
-	printf("\n%d\n", ft_printf("Hello, %010.8f", 42.1));
+	//printf("\n%d\n", printf("Hello, %010.8f%%. My name is%6.5s%c Temperature is %+d°C.", 42.1, "Aslan)@$!", '!', 32));
+	//printf("\n%d\n", ft_printf("Hello, %010.8f%%. My name is%6.5s%c Temperature is %+d°C.", 42.1, "Aslan)@$!", '!', 2));
+	printf("\n%d\n", ft_printf("Hello, %+010.8f%%. My name is%6.5s%c Temperature is % +d°C.", 42.1, "Aslan:@$!", '!', 2));
 	return (0);
 }
