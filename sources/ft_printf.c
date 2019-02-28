26c17b47f6ac5b4b432d2c65e3d402c6321a80b8
4slan <amalsago@student.42.fr>
2019-02-28 14:55:51 +0100

    removing while loop before if condition and placed buffer filling part in else condition. adding new condition to print buffer if it full. adding new condition to print buffer if it isn't empty

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/27 16:58:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/28 14:46:07 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@ int				ft_printf(const char *format, ...)
 	initialize_buffer(&buffer);
 	while (*format)
 	{
-		while (*format && *format != '%')
+		if (buffer.index == BUFF_SIZE - 1)		// When buffer is full, print it and reset
+			print_buffer(&buffer);
+		if (*format == '%')
+		{
+			format++;		// Пропустить и передать в parsing() строку после символа %
+			parsing(&format, &ap, &buffer);		// Запустить рабор (парсинг) строки
+		}
+		else
 		{
 			buffer.string[buffer.index] = *format;
 			buffer.index++;
-			format++;
 		}
-		if (*format++ == '%')
-		{
-			parsing(&format, &ap, &buffer);
-			return 1;
-		}/*
-		else
-			buffer.string[buffer.index++] = *format;
-		*/
 		format++;
 	}
+	if (buffer.index != 0)		// Print remaining content in buffer
+		print_buffer(&buffer);
 	return (buffer.length);
 }
