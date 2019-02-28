0da98cd0bdb76d6f6813c826a81c0cf42b874f03
4slan <amalsago@student.42.fr>
2019-02-28 14:58:09 +0100

    definition of types functions

--- sources/types.c
+++ sources/types.c
@@ -6,13 +6,38 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/28 10:21:00 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/02/28 14:46:13 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void			unknown(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_d(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_c(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
 void	type_f(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_s(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			percent(const char **format, t_placeholder *placeholder)
 {
 	placeholder->type = **format;
 }
