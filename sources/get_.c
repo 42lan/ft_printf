40d3e4d4b28991f1c5b2cf2b0990d383429fe67f
4slan <amalsago@student.42.fr>
2019-03-01 03:29:27 +0100

    adding rus comments

--- sources/get_.c
+++ sources/get_.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 15:18:09 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/01 01:51:21 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/01 03:27:16 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ void			get_int(const char **format, t_placeholder *placeholder)
 
 	nb = 0;
 	precision = 0;
-	if (**format == '.')
+	if (**format == '.') // Если текущий символ это '.' то значит следует точность
 	{
-		precision = 1;
-		(*format)++;
+		precision = 1; // Установить значение 1 для провери в конце функции
+		(*format)++; // Пропускаем символ '.' для определения целого числа
 	}
-	while (ft_isdigit(**format))
+	while (ft_isdigit(**format)) // Пока текущий символ это цифра то определить значение числа
 	{
 		nb *= 10;
 		nb = nb + (**format - '0');
 		(*format)++;
 	}
-	if (precision)
+	if (precision) // Если переменная precision равна 1 то установить целое число в поле precision структуры
 		placeholder->specs->precision = nb;
-	else
+	else  // Иначе установить определенное число в поле ширнины
 		placeholder->specs->width = nb;
 }
