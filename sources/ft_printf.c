a9eb5e5396771a4b21510fa8029116fe688d5d28
4slan <amalsago@student.42.fr>
2019-03-01 01:48:39 +0100

    commenting output part; adding extraction of char * to test; adding va_end()

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/02/28 14:46:07 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/01 01:44:21 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,20 @@ int				ft_printf(const char *format, ...)
 {
 	va_list		ap;
 	t_buffer	buffer;
+	//char		*s;
 
 	va_start(ap, format);
 	initialize_buffer(&buffer);
 	while (*format)
 	{
-		if (buffer.index == BUFF_SIZE - 1)		// When buffer is full, print it and reset
-			print_buffer(&buffer);
+		//if (buffer.index == BUFF_SIZE - 1)		// When buffer is full, print it and reset
+		//	print_buffer(&buffer);
 		if (*format == '%')
 		{
 			format++;		// Пропустить и передать в parsing() строку после символа %
 			parsing(&format, &ap, &buffer);		// Запустить рабор (парсинг) строки
+			//s = va_arg(ap, char *);
+			//ft_putendl(s);
 		}
 		else
 		{
@@ -35,7 +38,8 @@ int				ft_printf(const char *format, ...)
 		}
 		format++;
 	}
-	if (buffer.index != 0)		// Print remaining content in buffer
-		print_buffer(&buffer);
+	//if (buffer.index != 0)		// Print remaining content in buffer
+	//	print_buffer(&buffer);
+	va_end(ap);
 	return (buffer.length);
 }
