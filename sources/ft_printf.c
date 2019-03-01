1a1c1f2f80ab781985e5e013f14125d9e123c668
4slan <amalsago@student.42.fr>
2019-03-01 03:29:15 +0100

    commenting output of buffer and rus comment added

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/01 03:11:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/01 03:27:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@ int				ft_printf(const char *format, ...)
 	initialize_buffer(&buffer);
 	while (*format)
 	{
-		if (buffer.index == BUFF_SIZE - 1)		// When buffer is full, print it and reset
-			print_buffer(&buffer);
+		// if (buffer.index == BUFF_SIZE - 1) // When buffer is full, print it and reset
+		// 	print_buffer(&buffer);
 		if (*format == '%')
 		{
-			format++;		// Пропустить и передать в parsing() строку после символа %
-			parsing(&format, &ap, &buffer);		// Запустить рабор (парсинг) строки
+			format++; // Пропустить и передать в parsing() строку после символа %
+			parsing(&format, &ap, &buffer); // Запустить рабор (парсинг) строки
 		}
 		else
-			buffer.string[buffer.index++] = *format;
+			buffer.string[buffer.index++] = *format; // Записать в буффер текущий символ
 		format++;
 	}
-	if (buffer.index != 0)		// Если индекс буффера не равен нулю, то вывести оставшееся содержимое
-		print_buffer(&buffer);
+	// if (buffer.index != 0) // Если индекс буффера не равен нулю, то вывести оставшееся содержимое
+	// 	print_buffer(&buffer);
 	va_end(ap);
 	return (buffer.length);
 }
