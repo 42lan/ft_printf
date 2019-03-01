3dcbcccbdbfe6f52afe37ba5a6696a8230321b6e
4slan <amalsago@student.42.fr>
2019-03-01 03:30:39 +0100

    removing commented type_*()

--- sources/types.c
+++ sources/types.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/01 01:44:26 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/01 03:27:25 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,3 @@ void			get_type(const char **format, t_placeholder *placeholder)
 {
 	placeholder->type = **format;
 }
-
-/*
-void			type_d(const char **format, t_placeholder *placeholder)
-{
-	placeholder->type = **format;
-}
-
-void			type_c(const char **format, t_placeholder *placeholder)
-{
-	placeholder->type = **format;
-}
-
-void	type_f(const char **format, t_placeholder *placeholder)
-{
-	placeholder->type = **format;
-}
-
-void			type_s(const char **format, t_placeholder *placeholder)
-{
-	placeholder->type = **format;
-}
-
-void			percent(const char **format, t_placeholder *placeholder)
-{
-	placeholder->type = **format;
-}
-*/
