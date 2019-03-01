520dba9fb82e9c80e4a6f638b2f4ac374266ef53
4slan <amalsago@student.42.fr>
2019-03-01 03:12:06 +0100

    incrementing index while assignement actual char from format to buffer.string

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/01 01:44:21 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/01 03:11:25 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,24 @@ int				ft_printf(const char *format, ...)
 {
 	va_list		ap;
 	t_buffer	buffer;
-	//char		*s;
 
 	va_start(ap, format);
 	initialize_buffer(&buffer);
 	while (*format)
 	{
-		//if (buffer.index == BUFF_SIZE - 1)		// When buffer is full, print it and reset
-		//	print_buffer(&buffer);
+		if (buffer.index == BUFF_SIZE - 1)		// When buffer is full, print it and reset
+			print_buffer(&buffer);
 		if (*format == '%')
 		{
 			format++;		// Пропустить и передать в parsing() строку после символа %
 			parsing(&format, &ap, &buffer);		// Запустить рабор (парсинг) строки
-			//s = va_arg(ap, char *);
-			//ft_putendl(s);
 		}
 		else
-		{
-			buffer.string[buffer.index] = *format;
-			buffer.index++;
-		}
+			buffer.string[buffer.index++] = *format;
 		format++;
 	}
-	//if (buffer.index != 0)		// Print remaining content in buffer
-	//	print_buffer(&buffer);
+	if (buffer.index != 0)		// Если индекс буффера не равен нулю, то вывести оставшееся содержимое
+		print_buffer(&buffer);
 	va_end(ap);
 	return (buffer.length);
 }
