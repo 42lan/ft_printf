83e5fdfd16c8825c573958ef1f39363536c4c2e1
4slan <amalsago@student.42.fr>
2019-03-01 03:29:51 +0100

    adding rus comments

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 09:25:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/01 01:44:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/01 03:27:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void				parsing(const char **format, va_list *ap, t_buffer *buffer)
 	(void)buffer;
 	t_placeholder	*placeholder;
 
-	placeholder = initialize_placeholder();		// Инициализация плэйсхолдера
-	while (**format && !(placeholder->type))	// Пока читаю символы И пока не устоновлен тип плейсхолдера
+	placeholder = initialize_placeholder(); // Инициализация плэйсхолдера
+	while (**format && !(placeholder->type)) // Пока читаю символы И пока не устоновлен тип плейсхолдера
 	{
-		if (ft_isprint(**format))	// Является текущий символ символом между ' ' и '~'
-			jump_table[**format - ' '](format, placeholder);	// Если да, то определить и вызвать отосящююся функцию передав строку и структуру плейсхолдера
+		if (ft_isprint(**format)) // Является текущий символ символом между ' ' и '~'
+			jump_table[**format - ' '](format, placeholder); // Если да, то определить и вызвать отосящююся функцию передав строку и структуру плейсхолдера
 	}
 	print_placeholder(placeholder);
 }
