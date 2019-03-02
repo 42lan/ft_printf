0aed424b2356ec23c950801a08a2035a1e2dc2c8
4slan <amalsago@student.42.fr>
2019-03-02 14:49:59 +0100

    removin old commented code; typo in comment corrected

--- sources/flags.c
+++ sources/flags.c
@@ -6,37 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:00:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/01 03:27:14 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/02 14:41:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-/*
-int		is_flag(const char c)
-{
-	return (c == ' ' || c == '#' || c == '+' || c == '-' || c == '0');
-}
-*/
-/*
-** Checking if passed character in argument is one of the flags and
-** setting up the corresponding bit to 1
-*/
-/*
-void		set_flag(const char c, t_flags *flags) // Может быть второй аргумент будет сразу флагом?
-{
-	if (c == ' ')
-		flags->space = 1;
-	else if (c == '#')
-		flags->hash = 1;
-	else if (c == '+')
-		flags->plus = 1;
-	else if (c == '-')
-		flags->minus = 1;
-	else if (c == '0')
-		flags->zero = 1;
-}
-*/
 
 void			flag_space(const char **format, t_placeholder *placeholder)
 {
@@ -53,7 +27,7 @@ void			flag_hash(const char **format, t_placeholder *placeholder)
 void			flag_plus(const char **format, t_placeholder *placeholder)
 {
 	placeholder->specs->flags->plus = 1;
-	placeholder->specs->flags->space = 0; // Флаг '+' превосходить флаг ' '
+	placeholder->specs->flags->space = 0; // Флаг '+' превосходит флаг ' '
 	(*format)++;
 }
 
