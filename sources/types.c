b607ed00158f2ef8fe6dacda2c3c5fee5ef4745c
4slan <amalsago@student.42.fr>
2019-03-02 14:56:47 +0100

    types functions are defined

--- sources/types.c
+++ sources/types.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/01 03:27:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/02 14:42:03 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,57 @@ void			unknown(const char **format, t_placeholder *placeholder)
 }
 
 void			get_type(const char **format, t_placeholder *placeholder)
+{
+	ft_putendl("type %");
+	placeholder->type = **format;
+}
+
+void			type_c(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_d(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_f(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_i(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_o(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_p(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_s(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_u(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_x(const char **format, t_placeholder *placeholder)
+{
+	placeholder->type = **format;
+}
+
+void			type_X(const char **format, t_placeholder *placeholder)
 {
 	placeholder->type = **format;
 }
