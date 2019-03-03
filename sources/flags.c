804715247ac88e93f03de065b1b713aa1bfc7f2e
4slan <amalsago@student.42.fr>
2019-03-03 17:30:04 +0100

    t_placeholder renamed into t_info; placeholder pointer renamed into info

--- sources/flags.c
+++ sources/flags.c
@@ -6,39 +6,39 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:00:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/02 14:41:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/03 17:26:56 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			flag_space(const char **format, t_placeholder *placeholder)
+void			flag_space(const char **format, t_info *info)
 {
-	placeholder->specs->flags->space = 1;
+	info->specs->flags->space = 1;
 	(*format)++;
 }
 
-void			flag_hash(const char **format, t_placeholder *placeholder)
+void			flag_hash(const char **format, t_info *info)
 {
-	placeholder->specs->flags->hash = 1;
+	info->specs->flags->hash = 1;
 	(*format)++;
 }
 
-void			flag_plus(const char **format, t_placeholder *placeholder)
+void			flag_plus(const char **format, t_info *info)
 {
-	placeholder->specs->flags->plus = 1;
-	placeholder->specs->flags->space = 0; // Флаг '+' превосходит флаг ' '
+	info->specs->flags->plus = 1;
+	info->specs->flags->space = 0; // Флаг '+' превосходит флаг ' '
 	(*format)++;
 }
 
-void			flag_minus(const char **format, t_placeholder *placeholder)
+void			flag_minus(const char **format, t_info *info)
 {
-	placeholder->specs->flags->minus = 1;
+	info->specs->flags->minus = 1;
 	(*format)++;
 }
 
-void			flag_zero(const char **format, t_placeholder *placeholder)
+void			flag_zero(const char **format, t_info *info)
 {
-	placeholder->specs->flags->zero = 1;
+	info->specs->flags->zero = 1;
 	(*format)++;
 }
