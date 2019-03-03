7b95651c687d9ba4b41b7723d1a56fc4955c9e0b
4slan <amalsago@student.42.fr>
2019-03-03 17:37:34 +0100

    t_placeholder *placeholder becomes t_info *info

--- sources/types.c
+++ sources/types.c
@@ -6,69 +6,69 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/02 14:42:03 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/03 17:27:07 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			unknown(const char **format, t_placeholder *placeholder)
+void			unknown(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			get_type(const char **format, t_placeholder *placeholder)
+void			get_type(const char **format, t_info *info)
 {
 	ft_putendl("type %");
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_c(const char **format, t_placeholder *placeholder)
+void			type_c(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_d(const char **format, t_placeholder *placeholder)
+void			type_d(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_f(const char **format, t_placeholder *placeholder)
+void			type_f(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_i(const char **format, t_placeholder *placeholder)
+void			type_i(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_o(const char **format, t_placeholder *placeholder)
+void			type_o(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_p(const char **format, t_placeholder *placeholder)
+void			type_p(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_s(const char **format, t_placeholder *placeholder)
+void			type_s(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_u(const char **format, t_placeholder *placeholder)
+void			type_u(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_x(const char **format, t_placeholder *placeholder)
+void			type_x(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
 
-void			type_X(const char **format, t_placeholder *placeholder)
+void			type_X(const char **format, t_info *info)
 {
-	placeholder->type = **format;
+	info->type = **format;
 }
