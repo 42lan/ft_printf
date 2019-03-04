37202acf3b33d307b2f926b4ab3305b71f897289
4slan <amalsago@student.42.fr>
2019-03-04 16:53:27 +0100

    modifying argument va_list * to t_info of parsing(); calling initializa_specifiers to reset their values; setting type to 0

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,19 +6,18 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 09:25:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/03 17:27:03 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/04 16:49:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void				parsing(const char **format, va_list *ap, t_buffer *buffer)
+void				parsing(const char **format, t_info *info, t_buffer *buffer)
 {
-	(void)ap;
 	(void)buffer;
-	t_info	*info;
 
-	info = initialize_info(); // Инициализация плэйсхолдера
+	initialize_specifiers(info->specs);
+	info->type = 0;
 	while (**format && !(info->type)) // Пока читаю символы И пока не устоновлен тип плейсхолдера
 	{
 		if (ft_isprint(**format)) // Является текущий символ символом между ' ' и '~'
