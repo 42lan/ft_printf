227216d6dce9db2ff9ea53b550dc5a1b47464fd7
4slan <amalsago@student.42.fr>
2019-03-04 19:16:18 +0100

    removing declaration of t_buffer buffer and initialize_buffer() because buffer is in t_info

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/04 16:48:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/04 19:12:57 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,24 @@ int					ft_printf(const char *format, ...)
 {
 	// va_list			ap;
 	t_info			*info;
-	t_buffer		buffer;
 
 	info = initialize_info();
 	va_start(info->ap, format);
-	initialize_buffer(&buffer);
 	while (*format)
 	{
-		// if (buffer.index == BUFF_SIZE - 1) // When buffer is full, print it and reset
-		//  	print_buffer(&buffer);
+		if (info->buffer.index == BUFF_SIZE - 1) // When buffer is full, print it and reset
+			print_buffer(&info->buffer);
 		if (*format == '%')
 		{
 			format++; // Пропустить и передать в parsing() строку после символа %
-			parsing(&format, info, &buffer); // Запустить разбор (парсинг) строки
+			parsing(&format, info); // Запустить рабор (парсинг) строки
 		}
 		else
-			buffer.content[buffer.index++] = *format; // Записать в буффер текущий символ
+			info->buffer.content[info->buffer.index++] = *format; // Записать в буффер текущий символ
 		format++;
 	}
-	// if (buffer.index != 0) // Если индекс буффера не равен нулю, то вывести оставшееся содержимое
-	// 	print_buffer(&buffer);
+	if (info->buffer.index != 0) // Если индекс буффера не равен нулю, то вывести оставшееся содержимое
+		print_buffer(&info->buffer);
 	va_end(info->ap);
-	return (buffer.length);
+	return (info->buffer.length);
 }
