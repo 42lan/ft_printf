080a158e7688c97a80804f094efd805b2b5188d9
4slan <amalsago@student.42.fr>
2019-03-04 16:51:31 +0100

    declaration of t_info* instead t_info

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/03 17:26:57 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/04 16:48:58 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,27 @@
 int					ft_printf(const char *format, ...)
 {
 	// va_list			ap;
-	t_info	info;
+	t_info			*info;
+	t_buffer		buffer;
 
-	va_start(info.ap, format);
-	initialize_buffer(&info.buffer);
+	info = initialize_info();
+	va_start(info->ap, format);
+	initialize_buffer(&buffer);
 	while (*format)
 	{
-		// if (info.buffer.index == BUFF_SIZE - 1) // When buffer is full, print it and reset
-		// 	print_buffer(&info.buffer);
+		// if (buffer.index == BUFF_SIZE - 1) // When buffer is full, print it and reset
+		//  	print_buffer(&buffer);
 		if (*format == '%')
 		{
 			format++; // Пропустить и передать в parsing() строку после символа %
-			parsing(&format, &info.ap, &info.buffer); // Запустить рабор (парсинг) строки
+			parsing(&format, info, &buffer); // Запустить разбор (парсинг) строки
 		}
 		else
-			info.buffer.content[info.buffer.index++] = *format; // Записать в буффер текущий символ
+			buffer.content[buffer.index++] = *format; // Записать в буффер текущий символ
 		format++;
 	}
-	// if (info.buffer.index != 0) // Если индекс буффера не равен нулю, то вывести оставшееся содержимое
-	// 	print_buffer(&info.buffer);
-	va_end(info.ap);
-	return (info.buffer.length);
+	// if (buffer.index != 0) // Если индекс буффера не равен нулю, то вывести оставшееся содержимое
+	// 	print_buffer(&buffer);
+	va_end(info->ap);
+	return (buffer.length);
 }
