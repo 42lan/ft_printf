931df38112f691a60730f24720ca6b48e17ad0b2
4slan <amalsago@student.42.fr>
2019-03-13 21:27:18 +0100

    adding new arg number to create condition if number is > 0; adding == 0 to if in while loop

--- sources/specs.c
+++ sources/specs.c
@@ -6,21 +6,23 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 13:40:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/13 18:17:28 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/13 21:24:58 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void        specs_handle(t_info *info, int nb_digits)
+void        specs_handle(t_info *info, int number, unsigned nb_digits)
 {
     int		nb_spaces;
 
 	nb_spaces = info->specs->width - nb_digits;
 	// printf(">%d - %d = %d<\n", info->specs->width, nb_digits, info->specs->width - nb_digits);
+	if (number > 0 && info->specs->flags->plus)
+		nb_spaces--;
 	while (nb_spaces--)
 	{
-		if (!buffer_full(&info->buffer))
+		if (buffer_full(&info->buffer) == 0)
 			fill_buffer(&info->buffer, ' ');
 		else
 		{
