7cefe3ffbd985bdec6a2137d80529e38c6621107
4slan <amalsago@student.42.fr>
2019-03-13 18:22:19 +0100

    definition of buffer_full() and fill_buffer(); removing buffer->length += buffer->index fropm print_buffer

--- sources/buffer.c
+++ sources/buffer.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 04:48:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/04 16:48:56 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/13 18:16:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,19 @@
 void		print_buffer(t_buffer *buffer)
 {
 	write(1, buffer->content, buffer->index); // Вывести на экран строку в количестве индекса
-	buffer->length += buffer->index; // Заполнить счётчик количества символов
 	buffer->index = 0; // Передвинуть индекс на 0
+}
+
+int			buffer_full(t_buffer *buffer)
+{
+	return (buffer->index == BUFF_SIZE - 1);
+}
+
+void		fill_buffer(t_buffer *buffer, char c)
+{
+	if (!buffer_full(buffer))
+	{
+		buffer->content[buffer->index++] = c;
+		buffer->length++; // Увеличить счётчик количества символов в строке
+	}
 }
