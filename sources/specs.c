1ab6fbc518752d59588e85336ee93c514571a07d
4slan <amalsago@student.42.fr>
2019-03-16 11:47:12 +0100

    nb_len and nb_spaces becomes unsigned; rearengement of while loop in width_handler()

--- sources/specs.c
+++ sources/specs.c
@@ -6,15 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 13:40:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/15 10:42:40 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/16 11:42:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void        width_handler(t_info *info, int number, int nb_len)
+void			width_handler(t_info *info, int number, unsigned nb_len)
 {
-    int		nb_spaces;
+    unsigned	nb_spaces;
 
 	if (info->specs->precision > nb_len)
 	{
@@ -30,12 +30,12 @@ void        width_handler(t_info *info, int number, int nb_len)
 			nb_spaces--;
 	while (nb_spaces-- > 0)
 	{
-		if (buffer_full(&info->buffer) == 0)
-			fill_buffer(&info->buffer, ' ');
-		else
+		if (buffer_full(&info->buffer) == 1)
 		{
 			print_buffer(&info->buffer);
 			nb_spaces++;
 		}
+		else
+			fill_buffer(&info->buffer, ' ');
 	}
 }
