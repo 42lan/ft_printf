c8cee08f7a4725546934d7094ff657a2b39fff2c
4slan <amalsago@student.42.fr>
2019-03-16 11:36:04 +0100

    rearengement of funcitons

--- sources/buffer.c
+++ sources/buffer.c
@@ -6,11 +6,16 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 04:48:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/15 11:18:57 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/16 10:58:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int			buffer_full(t_buffer *buffer)
+{
+	return (buffer->index == BUFF_SIZE - 1);
+}
 
 void		print_buffer(t_buffer *buffer)
 {
@@ -18,14 +23,8 @@ void		print_buffer(t_buffer *buffer)
 	buffer->index = 0; // Передвинуть индекс на 0
 }
 
-int			buffer_full(t_buffer *buffer)
-{
-	return (buffer->index == BUFF_SIZE - 1);
-}
-
 void		fill_buffer(t_buffer *buffer, char c)
 {
-
 	if (buffer_full(buffer) == 1) // Если буффер переполнен, то вывести (и обнулить) его
 		print_buffer(buffer);
 	buffer->content[buffer->index++] = c;
