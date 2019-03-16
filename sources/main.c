569839546010cf5c1ba3f3ce3e0668b273f023d0
4slan <amalsago@student.42.fr>
2019-03-16 18:32:24 +0100

    new test

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/15 10:55:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/16 15:21:31 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int			main(void)
 	//printf("\n%d\n", printf("My name is%6.5s%c Hello, %+010.8f%%. Temperature is %+d°C.", "Aslan:@$!", '!', 42.1, -2));
 	// printf("\n%d\n", ft_printf("My name is%6.5s%c Hello, %+010.8f%%. Temperature is %+d°C.", "Aslan:@$!", '!', 42.1, -2));
 	// printf("\n->%d\n",  printf("HelloHelloHelloHelloHelloHelloHelloHello %-d", 42));
-	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",		 printf("Hello! Here is %d.", 42));
-	printf("\n\033[36m[ FT_PRINTF ]\t-> Return: %d\033[0m\n", ft_printf("Hello! Here is %d.", 42));
-	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",		 printf("Hello! Here is %d.", -42));
-	printf("\n\033[36m[ FT_PRINTF ]\t-> Return: %d\033[0m\n", ft_printf("Hello! Here is %d.", -42));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",		 printf("Hello! Here is %0+10d.", 214748));
+	printf("\n\033[36m[ FT_PRINTF ]\t-> Return: %d\033[0m\n", ft_printf("Hello! Here is %0+10d.", 214748));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",		 printf("Hello! Here is %0+10d.", -214748));
+	printf("\n\033[36m[ FT_PRINTF ]\t-> Return: %d\033[0m\n", ft_printf("Hello! Here is %0+10d.", -214748));
 	return (0);
 }
