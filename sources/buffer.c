5a2b2e34051d6ef9243cecb17e8ba88f18b1aee5
Aslan MALSAGOV <amalsago@e3r1p14.42.fr>
2019-03-22 19:30:01 +0100

    fill_buffer() renmaed into write_char(); definition of write_str()

--- sources/buffer.c
+++ sources/buffer.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 04:48:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/16 11:39:54 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/22 11:08:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,16 @@ void	print_buffer(t_buffer *buffer)
 	buffer->index = 0; // Передвинуть индекс на 0
 }
 
-void	fill_buffer(t_buffer *buffer, char c)
+void	write_char(t_buffer *buffer, char c)
 {
 	if (buffer_full(buffer) == 1) // Если буффер переполнен, то вывести (и обнулить) его
 		print_buffer(buffer);
 	buffer->content[buffer->index++] = c;
 	buffer->length++; // Увеличить счётчик количества символов в строке
+}
+
+void	write_str(t_buffer *buffer, char *str, int length)
+{
+	while (str && *str && length--)
+		write_char(buffer, *str++);
 }
