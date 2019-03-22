8cb8c4a687107594e1bf925a4ce49a18f4d0ccdf
4slan <amalsago@student.42.fr>
2019-03-22 19:37:27 +0100

    definition of type_c()

---
+++ sources/type_c.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_c.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/22 10:26:29 by amalsago          #+#    #+#             */
+/*   Updated: 2019/03/22 14:27:12 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void			type_c(const char **format, t_info *info)
+{
+	char		c;
+	t_data		data;
+
+	info->type = **format;
+	c = (char)va_arg(info->ap, int);
+	data.str = &c;
+	data.length = 1;
+	apply_specs(info, &data);
+	// write_char(&info->buffer, c);
+}
