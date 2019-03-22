6d17c2725f3e55d0eefb08c5dc0c38df3fc9637c
4slan <amalsago@student.42.fr>
2019-03-22 19:37:14 +0100

    specs.c renamed into specs_handler.c

---
+++ sources/specs_handler.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   specs_handler.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/22 11:24:59 by amalsago          #+#    #+#             */
+/*   Updated: 2019/03/22 18:49:29 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void		apply_specs(t_info *info, t_data *data)
+{
+	if (info->specs->flags->minus == 1)
+	{
+		put_prefix(info, data);
+		put_precision(info, data);
+		write_str(&info->buffer, data->str, data->length);
+		put_width(info, data);
+	}
+	else
+	{
+		put_width(info, data);
+		put_prefix(info, data);
+		put_precision(info, data);
+		write_str(&info->buffer, data->str, data->length);
+	}
+}
+
+void		put_width(t_info *info, t_data *data)
+{
+	int		width;
+
+	if (info->specs->width < data->length)
+		width = 0;
+	else if (info->specs->precision > data->length)
+	{
+		if (info->specs->flags->plus)
+			width = info->specs->width - info->specs->precision - 1;
+		else
+			width = info->specs->width - info->specs->precision;
+	}
+	else
+	{
+		width = info->specs->width - data->length;
+		if (info->specs->flags->plus)
+			width--;
+	}
+	while (width-- > 0)
+		write_char(&info->buffer, ' ');
+}
+
+void		put_precision(t_info *info, t_data *data)
+{
+	int		precision;
+
+	precision = info->specs->precision - data->length;
+	if (data->str[0] == '-')
+		precision++;
+	if (precision > 0)
+		while (precision-- > 0)
+			write_char(&info->buffer, '0');
+}
+
+void		put_prefix(t_info *info, t_data *data)
+{
+	(void)data;
+	if (info->specs->flags->plus == 1)
+		write_char(&info->buffer, '+');
+	else if (info->specs->flags->space == 1)
+		write_char(&info->buffer, ' ');
+}
