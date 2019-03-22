c5effe6d345300c794a5d93335c8f1694a5e980e
4slan <amalsago@student.42.fr>
2019-03-22 19:35:43 +0100

    removing is_placeholder(), is_width(), is_precision(), set_precision(), set_width()

--- sources/placeholder.c
+++ sources/placeholder.c
@@ -6,62 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:50:25 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/04 19:13:03 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/20 11:56:52 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-/*
-int		is_placeholder(const char *format)
-{
-	while (*format)
-	{
-		//if (!is_conversion_type(*format) && *(format + 1) == '%')
-		//	return (0);
-
-		if (is_conversion_type(*format))
-			return (1);
-		format++;
-	}
-	return (0);	// Нужно ли это ?
-}
-
-
-int		is_width(const char *str)
-{
-	while (*str && *str != '.') // while (*str && !is_precision(str)) OR while (*str != '.')
-	{
-		if (ft_isdigit(*str))
-			str++;
-		else
-			return (0);
-	}
-	return (1);
-}
-
-int		is_precision(const char *str)
-{
-	while (*str && !is_conversion_type(*str))// && !is_length(str)) au lieu de && !is_conversion_type()
-	{
-		if (ft_isdigit(*str))
-			str++;
-		else
-			return (0);
-	}
-	return (1);
-}
-
-void				set_precision(int precision, t_spec *specs)
-{
-	specs->precision = precision;
-}
-
-void				set_width(int width, t_spec *specs)
-{
-	specs->width = width;
-}
-*/
 
 void		print_bit_state(unsigned char c)
 {
