79b70cc1494d7014243cc34987e0835cbdb05707
Aslan MALSAGOV <amalsago@e3r1p14.42.fr>
2019-03-22 19:30:50 +0100

    removing old tests

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/16 15:21:31 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/22 18:50:41 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,11 @@
 
 int			main(void)
 {
-	//printf("\n%d\n", printf("Hello, %.5s%c", "World#$%^&*786ef", '!'));
-	//printf("\n%d\n", ft_printf("Hello, %-25%. %-10.5s%c", "World#$^&*786ef", '!'));
-	//printf("\n%d\n", printf("Hello, %010.8f%%. My name is%6.5s%c Temperature is %+d°C.", 42.1, "Aslan)@$!", '!', 32));
-	//printf("\n%d\n", printf("My name is%6.5s%c Hello, %+010.8f%%. Temperature is %+d°C.", "Aslan:@$!", '!', 42.1, -2));
-	// printf("\n%d\n", ft_printf("My name is%6.5s%c Hello, %+010.8f%%. Temperature is %+d°C.", "Aslan:@$!", '!', 42.1, -2));
-	// printf("\n->%d\n",  printf("HelloHelloHelloHelloHelloHelloHelloHello %-d", 42));
-	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",		 printf("Hello! Here is %0+10d.", 214748));
-	printf("\n\033[36m[ FT_PRINTF ]\t-> Return: %d\033[0m\n", ft_printf("Hello! Here is %0+10d.", 214748));
-	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",		 printf("Hello! Here is %0+10d.", -214748));
-	printf("\n\033[36m[ FT_PRINTF ]\t-> Return: %d\033[0m\n", ft_printf("Hello! Here is %0+10d.", -214748));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",	  printf("Hello %--10c.", '!'));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",	  ft_printf("Hello %--10c.", '!'));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",	  printf("Hello %--10.3s.", "WORLD123"));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",	  ft_printf("Hello %--10.3s.", "WORLD123"));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n",	  printf("Hello %---10.3d.", -42));
+	printf("\n\033[35m[ PRINTF ]\t-> Return: %d\033[0m\n", ft_printf("Hello %---10.3d.", -42));
 	return (0);
 }
