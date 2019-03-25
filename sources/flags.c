f2e369308de2bc7da4aa401af333b7f107b6423f
4slan <amalsago@student.42.fr>
2019-03-25 15:03:11 +0100

    resetting flag zero with minus

--- sources/flags.c
+++ sources/flags.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:00:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/04 16:48:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/25 12:30:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void			flag_plus(const char **format, t_info *info)
 void			flag_minus(const char **format, t_info *info)
 {
 	info->specs->flags->minus = 1;
+	info->specs->flags->zero = 0;
 	(*format)++;
 }
 
