b423236305b06c5fec28b625d7a2c950d94d172c
4slan <amalsago@student.42.fr>
2019-03-25 19:03:58 +0100

    definiton of type_s()

--- sources/type_s.c
+++ sources/type_s.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 14:37:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/23 12:49:29 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/25 19:00:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,45 @@
 void		type_s(const char **format, t_info *info)
 {
 	t_data		data;
+	int			i;
 
+	i = -1;
 	info->type = **format;
 	data.str = va_arg(info->ap, char *);
 	data.length = ft_strlen(data.str);
-	info->specs->flags->plus = 0;
-	if (info->specs->precision > 0) // Укрощение строки
-		// Невозможно перезаписать data.str так как она находится в RO части памяти
-		// Можно создать новую строку маллоком?
-	apply_specs(info, &data);
+	if (info->specs->flags->minus == 1)
+	{
+		while (++i < info->specs->precision)
+			write_char(&info->buffer, data.str[i]);
+	}
+	else
+	{
+		if (info->specs->precision == 0 && (info->specs->width < data.length && info->specs->width > data.length))
+		{
+			put_width_s(info, &data);
+			while (++i < data.length)
+				write_char(&info->buffer, data.str[i]);
+			
+		}
+		else if (info->specs->width <= data.length)
+		{
+			put_width_s(info, &data);
+			if (info->specs->precision < data.length)
+				while (++i < info->specs->precision)
+					write_char(&info->buffer, data.str[i]);
+			else
+				while (++i < data.length)
+					write_char(&info->buffer, data.str[i]);
+		}
+		else
+		{
+			put_width_s(info, &data);
+			if (info->specs->precision < data.length)
+				while (++i < info->specs->precision)
+					write_char(&info->buffer, data.str[i]);
+			else
+				while (++i < data.length)
+					write_char(&info->buffer, data.str[i]);
+		}
+	}
 }
