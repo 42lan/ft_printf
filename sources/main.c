684b247af690ec435840243c954497e7569769eb
4slan <amalsago@student.42.fr>
2019-03-26 19:24:29 +0100

    test for s

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/25 18:59:57 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/26 19:12:07 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int			main(void)
 {
-	printf("\n[ PRINTF ]\t-> Return: %d\n",	      printf("Hello %8s!", "World123"));
-	printf("\n[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("Hello %8s!", "World123"));
+	 printf("\n[ PRINTF ]\t-> Return: %d\n",	      printf("Hello %s!", "World123"));
+	 printf("\n[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("Hello %s!", "World123"));
+	printf("\n[ PRINTF ]\t-> Return: %d\n",	      printf("Hello %-s!", "World123"));
+	printf("\n[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("Hello %-s!", "World123"));
 	return (0);
 }
