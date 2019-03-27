48a1b3da5cb061c8878f41f44abca4ecf2bc7da1
4slan <amalsago@student.42.fr>
2019-03-27 19:41:15 +0100

    new tests

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/26 19:12:07 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/27 19:29:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int			main(void)
 {
-	 printf("\n[ PRINTF ]\t-> Return: %d\n",	      printf("Hello %s!", "World123"));
-	 printf("\n[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("Hello %s!", "World123"));
-	printf("\n[ PRINTF ]\t-> Return: %d\n",	      printf("Hello %-s!", "World123"));
-	printf("\n[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("Hello %-s!", "World123"));
+	 printf("\n[ PRINTF ]\t-> Return: %d\n",	   printf("Hello %10.14s!", NULL));
+	 printf("\n[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("Hello %10.14s!", NULL));
+	printf("\n[ PRINTF ]\t-> Return: %d\n",	      printf("Hello %-10.14s!", NULL));
+	printf("\n[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("Hello %-10.14s!", NULL));
 	return (0);
 }
