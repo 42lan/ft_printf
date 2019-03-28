416b9e44675940e2bc8fa2f9cd96a5597ccbece2
4slan <amalsago@student.42.fr>
2019-03-28 12:21:22 +0100

    removing type_percent()

--- sources/types.c
+++ sources/types.c
@@ -6,18 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/22 15:04:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/28 10:27:40 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 void			unknown(const char **format, t_info *info)
-{
-	info->type = **format;
-}
-
-void			type_percent(const char **format, t_info *info)
 {
 	info->type = **format;
 }
