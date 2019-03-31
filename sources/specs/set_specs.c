a9e16045de28b35f33ca721d95c2e19df7753a4b
4slan <amalsago@student.42.fr>
2019-03-31 15:53:08 +0200

    get_number() placed here

--- sources/specs/set_specs.c
+++ sources/specs/set_specs.c
@@ -6,25 +6,39 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 09:45:05 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 11:30:48 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/31 15:38:16 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_printf.h"
 
-void		digit(const char **format, t_info *info)
+static int		get_number(const char **format)
+{
+	int			number;
+
+	number = 0;
+	while (ft_isdigit(**format)) // Пока текущий символ это цифра то определить значение числа
+	{
+		number *= 10;
+		number = number + (**format - '0');
+		(*format)++;
+	}
+	return (number);
+}
+
+void			digit(const char **format, t_info *info)
 {
 	if (*(*format - 1) != '.') // Если это не precision, то это width
 		set_width(format, info);
 }
 
-void		set_width(const char **format, t_info *info)
+void			set_width(const char **format, t_info *info)
 {
 	info->specs->width = get_number(format);
 }
 
-void		set_precision(const char **format, t_info *info)
+void			set_precision(const char **format, t_info *info)
 {
 	(*format)++;
 	info->specs->flags->point = 1;
