3935d8e2e3616addb3285fdec4c082eed23d3390
4slan <amalsago@student.42.fr>
2019-04-03 12:29:27 +0200

    indenting code

--- libft/sources/ft_strcat.c
+++ libft/sources/ft_strcat.c
@@ -6,16 +6,16 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 18:05:14 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 15:47:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/31 16:57:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcat(char *s1, const char *s2)
+char		*ft_strcat(char *s1, const char *s2)
 {
-	int	i;
-	int	len;
+	int		i;
+	int		len;
 
 	i = 0;
 	len = ft_strlen(s1);
