d4cc71fc57ef003187a4aeafb978119b7c0c62bf
4slan <amalsago@student.42.fr>
2019-04-03 12:29:47 +0200

    indenting code

--- libft/sources/ft_strchr.c
+++ libft/sources/ft_strchr.c
@@ -6,15 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 11:20:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 15:47:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/31 16:57:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char		*ft_strchr(const char *s, int c)
 {
-	int	i;
+	int		i;
 
 	i = 0;
 	while (s[i] != '\0')
