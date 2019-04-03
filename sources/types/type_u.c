7744aeef2df496efa1a5efb650a7242e83a13124
4slan <amalsago@student.42.fr>
2019-04-03 12:37:59 +0200

    first definition of type_u()

--- sources/types/type_u.c
+++ sources/types/type_u.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/31 16:11:50 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 16:31:38 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/31 18:59:46 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,17 @@
 void			type_u(const char **format, t_info *info)
 {
 	info->specs->flags->hash = 0;
+	info->type = **format;
+	unsigned	number;
+	t_data		data;
+
+	info->type = **format;
+	number = va_arg(info->ap, unsigned int);
+	info->specs->flags->hash = 0;
+	data.str = ft_itoa(ABS(number));
+	data.length = ft_nblen(number);
+	data.negative = 0;
+	if (number < 0)
+		data.negative = 1;
+	apply_specs(info, &data);
 }
