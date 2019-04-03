e2bdb371a1da9c4ade4baf589a8ab04b42f30c30
4slan <amalsago@student.42.fr>
2019-04-03 12:35:23 +0200

    code indented; removing void casting in length_L()

--- sources/specs/set_length.c
+++ sources/specs/set_length.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 17:04:35 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 11:43:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/31 16:50:58 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			length_h(const char **format, t_info *info)
+void	length_h(const char **format, t_info *info)
 {
 	info->specs->length = BIT_1; // BIT_1 = h
 	if (*(*format + 1) == 'h') // hh
@@ -23,7 +23,7 @@ void			length_h(const char **format, t_info *info)
 	(*format)++;
 }
 
-void			length_l(const char **format, t_info *info)
+void	length_l(const char **format, t_info *info)
 {
 	info->specs->length = BIT_3; // BIT_3 = l
 	if (*(*format + 1) == 'l') // ll
@@ -34,11 +34,9 @@ void			length_l(const char **format, t_info *info)
 	(*format)++;
 }
 
-void			length_L(const char **format, t_info *info)
+void	length_L(const char **format, t_info *info)
 {
 	ft_putendl("Length -> L");
 	info->specs->length = BIT_5; // BIT_5 = L
-	(void)format;
-	(void)info;
 	(*format)++;
 }
