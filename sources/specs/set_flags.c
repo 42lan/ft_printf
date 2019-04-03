e0605514d9446fabfa91841d1ffc090f703c9725
4slan <amalsago@student.42.fr>
2019-04-03 12:34:39 +0200

    code intented

--- sources/specs/set_flags.c
+++ sources/specs/set_flags.c
@@ -6,39 +6,39 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 05:00:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 11:25:57 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/03/31 16:50:10 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			flag_space(const char **format, t_info *info)
+void	flag_space(const char **format, t_info *info)
 {
 	info->specs->flags->space = 1;
 	(*format)++;
 }
 
-void			flag_hash(const char **format, t_info *info)
+void	flag_hash(const char **format, t_info *info)
 {
 	info->specs->flags->hash = 1;
 	(*format)++;
 }
 
-void			flag_plus(const char **format, t_info *info)
+void	flag_plus(const char **format, t_info *info)
 {
 	info->specs->flags->plus = 1;
 	info->specs->flags->space = 0; // Флаг '+' превосходит флаг ' ' так что обнуляем ' '
 	(*format)++;
 }
 
-void			flag_minus(const char **format, t_info *info)
+void	flag_minus(const char **format, t_info *info)
 {
 	info->specs->flags->minus = 1;
 	info->specs->flags->zero = 0;
 	(*format)++;
 }
 
-void			flag_zero(const char **format, t_info *info)
+void	flag_zero(const char **format, t_info *info)
 {
 	info->specs->flags->zero = 1;
 	(*format)++;
