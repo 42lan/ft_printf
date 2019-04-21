f491c2861d122b857aedbdbb4840301922df49e8
4slan <amalsago@student.42.fr>
2019-04-21 19:41:04 +0200

    removing type_o(), type_x() and type_X()

--- sources/types/types.c
+++ sources/types/types.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 16:56:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/21 10:54:27 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,7 @@ void	type_f(const char **format, t_info *info)
 	info->type = **format;
 }
 
-void	type_o(const char **format, t_info *info)
-{
-	info->type = **format;
-}
-
 void	type_p(const char **format, t_info *info)
-{
-	info->type = **format;
-}
-
-void	type_x(const char **format, t_info *info)
-{
-	info->type = **format;
-}
-
-void	type_X(const char **format, t_info *info)
 {
 	info->type = **format;
 }
