6b939b6c410e056864d67cb8d6ac3a6c8b2e8e2f
4slan <amalsago@student.42.fr>
2019-04-21 19:38:20 +0200

    removing length_L() and putting it in length_l(); length_j(), length_z() and length_t are defined

--- sources/specs/set_length.c
+++ sources/specs/set_length.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 17:04:35 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/15 09:46:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/21 09:52:27 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,34 @@ void	length_h(const char **format, t_info *info)
 
 void	length_l(const char **format, t_info *info)
 {
-	info->specs->length = LENGTH_L;
-	if (*(*format + 1) == 'l')
+	if (**format == 'L')
+		info->specs->length = LENGTH_LLL;
+	else
 	{
-		info->specs->length = LENGTH_LL;
-		(*format)++;
+		info->specs->length = LENGTH_L;
+		if (*(*format + 1) == 'l')
+		{
+			info->specs->length = LENGTH_LL;
+			(*format)++;
+		}
 	}
 	(*format)++;
 }
 
-void	length_L(const char **format, t_info *info)
+void	length_j(const char **format, t_info *info)
 {
-	ft_putendl("Length -> L");
-	info->specs->length = LENGTH_LLL;
+	info->specs->length = LENGTH_J;
+	(*format)++;
+}
+
+void	length_z(const char **format, t_info *info)
+{
+	info->specs->length = LENGTH_Z;
+	(*format)++;
+}
+
+void	length_t(const char **format, t_info *info)
+{
+	info->specs->length = LENGTH_T;
 	(*format)++;
 }
