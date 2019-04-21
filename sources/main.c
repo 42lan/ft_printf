815b3046274e50feaf6f846166c13947726914a5
4slan <amalsago@student.42.fr>
2019-04-21 19:37:30 +0200

    new test

--- sources/main.c
+++ sources/main.c
@@ -6,15 +6,16 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/16 19:35:48 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/21 19:16:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int			main(void)
 {
-	printf("\t[ PRINTF ]\t-> Return: %d\n", printf("%lu", -43));
-	printf("\t[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("%lu", -43));
+	printf("\t[ PRINTF ]\t-> Return: %d\n",	      printf("%#5.5x.", 42));
+	printf("\t[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("%#5.5x.", 42));
 	return (0);
 }
