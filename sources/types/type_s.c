54995ef97f0ca5559b9f8551c9b20e2f2a7f8efb
4slan <amalsago@student.42.fr>
2019-04-22 19:41:27 +0200

    removing comments

--- sources/types/type_s.c
+++ sources/types/type_s.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 14:37:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 16:55:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/22 19:41:17 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void			type_s(const char **format, t_info *info)
 	if (data.str == NULL)
 	{
 		data.str = "(null)";
-		data.length = ft_strlen(data.str);												//		s = "World123"
+		data.length = ft_strlen(data.str);
 		if (info->specs->flags->minus == 1)
 		{
 			write_str(&info->buffer, data.str, data.length);
@@ -37,15 +37,15 @@ void			type_s(const char **format, t_info *info)
 		}
 		return ;
 	}
-	data.length = ft_strlen(data.str);												//		s = "World123"
+	data.length = ft_strlen(data.str);
 	if (data.str == "")
 		data.length = 0;
-	if (info->specs->width == 0														// OK	width == Nan && precision ==.		%.s AND %-.s
-			&& (info->specs->flags->point == 1 && info->specs->precision == 0))
+	if (info->specs->width == 0
+		&& (info->specs->flags->point == 1 && info->specs->precision == 0))
 		return ;
-	if (info->specs->width <= data.length && info->specs->flags->point == 0)		// OK	width <= len (s)					%5s AND %-5s
+	if (info->specs->width <= data.length && info->specs->flags->point == 0)
 		write_str(&info->buffer, data.str, data.length);
-	else if (info->specs->width > data.length && info->specs->flags->point == 0)	// OK	width > len (s)						%15s AND %-15s
+	else if (info->specs->width > data.length && info->specs->flags->point == 0)
 	{
 		if (info->specs->flags->minus == 1)
 		{
@@ -58,37 +58,41 @@ void			type_s(const char **format, t_info *info)
 			write_str(&info->buffer, data.str, data.length);
 		}
 	}
-	else if (info->specs->width != 0												// width != 0 && precision == . || 0
-			&& (info->specs->flags->point == 0 || info->specs->precision == 0))		// %15.s AND %15.0s AND  %-15.s AND %-15.0s
+	else if (info->specs->width != 0
+			&& (info->specs->flags->point == 0 || info->specs->precision == 0))
 		put_width_s(info, &data);
-	else if (info->specs->width != 0 && info->specs->precision < data.length)		// width != 0 && precision < len(s)
-	{																				// %15.3s AND %-15.3s
+	else if (info->specs->width != 0 && info->specs->precision < data.length)
+	{
 		if (info->specs->flags->minus == 1)
 		{
-			write_str(&info->buffer, data.str, data.length - (data.length - info->specs->precision));
+			write_str(&info->buffer, data.str, data.length
+					- (data.length - info->specs->precision));
 			put_width_s(info, &data);
 		}
 		else
 		{
 			put_width_s(info, &data);
-			write_str(&info->buffer, data.str, data.length - (data.length - info->specs->precision));
+			write_str(&info->buffer, data.str,
+					data.length - (data.length - info->specs->precision));
 		}
 	}
-	else if (info->specs->width == 0 && info->specs->precision <= data.length)		// width == 0 && precision <= len(s)
-	{																				// %.3s AND %-.3s 
+	else if (info->specs->width == 0 && info->specs->precision <= data.length)
+	{
 		if (info->specs->flags->minus == 1)
 		{
-			write_str(&info->buffer, data.str, data.length - (data.length - info->specs->precision));
+			write_str(&info->buffer, data.str,
+					data.length - (data.length - info->specs->precision));
 			put_width_s(info, &data);
 		}
 		else
 		{
 			put_width_s(info, &data);
-			write_str(&info->buffer, data.str, data.length - (data.length - info->specs->precision));
+			write_str(&info->buffer, data.str,
+					data.length - (data.length - info->specs->precision));
 		}
 	}
-	else if (info->specs->width == 0 && info->specs->precision > data.length)		// width == 0 && preicison > len(s)
-	{																				// %.13s AND %-.13s 
+	else if (info->specs->width == 0 && info->specs->precision > data.length)
+	{
 		if (info->specs->flags->minus == 1)
 		{
 			write_str(&info->buffer, data.str, data.length);
