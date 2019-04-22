faf2866ca70fbbf1c384d367e96c1c05f3804af8
4slan <amalsago@student.42.fr>
2019-04-22 19:42:03 +0200

    normalization; calling free() at the end

--- sources/types/type_x.c
+++ sources/types/type_x.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/21 10:40:55 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/21 17:16:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/22 17:36:29 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ static void		get_ui(intmax_t *number, t_info *info)
 	else if (info->specs->length == LENGTH_L)
 		*number = (unsigned long int)va_arg(info->ap, unsigned long int);
 	else if (info->specs->length == LENGTH_LL)
-		*number = (unsigned long long int)va_arg(info->ap, unsigned long long int);
+		*number = (unsigned long long int)
+					va_arg(info->ap, unsigned long long int);
 	else if (info->specs->length == LENGTH_J)
 		*number = (uintmax_t)va_arg(info->ap, uintmax_t);
 	else if (info->specs->length == LENGTH_Z)
@@ -54,4 +55,5 @@ void			type_x(const char **format, t_info *info)
 	data.length = ft_nblen(number);
 	data.negative = 0;
 	apply_specs(info, &data);
+	free(data.str);
 }
