2098214f9dc98e47f44cfab25d305d79bb54b1e5
4slan <amalsago@student.42.fr>
2019-04-23 20:13:33 +0200

    trying to reduce lines

--- sources/types/type_s.c
+++ sources/types/type_s.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 14:37:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/22 19:41:17 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/23 20:10:26 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,14 @@ void			type_s(const char **format, t_info *info)
 	if (data.str == NULL)
 	{
 		data.str = "(null)";
-		data.length = ft_strlen(data.str);
+		if (info->specs->flags->point == 1 && info->specs->precision == 0)
+			data.length = 0;
+		else if (info->specs->precision > 0)
+		{
+			data.length = info->specs->precision;
+		}
+		else
+			data.length = ft_strlen(data.str);
 		if (info->specs->flags->minus == 1)
 		{
 			write_str(&info->buffer, data.str, data.length);
@@ -40,9 +47,6 @@ void			type_s(const char **format, t_info *info)
 	data.length = ft_strlen(data.str);
 	if (data.str == "")
 		data.length = 0;
-	if (info->specs->width == 0
-		&& (info->specs->flags->point == 1 && info->specs->precision == 0))
-		return ;
 	if (info->specs->width <= data.length && info->specs->flags->point == 0)
 		write_str(&info->buffer, data.str, data.length);
 	else if (info->specs->width > data.length && info->specs->flags->point == 0)
@@ -58,54 +62,57 @@ void			type_s(const char **format, t_info *info)
 			write_str(&info->buffer, data.str, data.length);
 		}
 	}
-	else if (info->specs->width != 0
-			&& (info->specs->flags->point == 0 || info->specs->precision == 0))
-		put_width_s(info, &data);
-	else if (info->specs->width != 0 && info->specs->precision < data.length)
+	else if (info->specs->width == 0)
 	{
-		if (info->specs->flags->minus == 1)
+		if ((info->specs->flags->point == 1 && info->specs->precision == 0))
+			return ;
+		if (info->specs->precision <= data.length)
 		{
-			write_str(&info->buffer, data.str, data.length
-					- (data.length - info->specs->precision));
-			put_width_s(info, &data);
+			if (info->specs->flags->minus == 1)
+			{
+				write_str(&info->buffer, data.str,
+					data.length - (data.length - info->specs->precision));
+				put_width_s(info, &data);
+			}
+			else
+			{
+				put_width_s(info, &data);
+				write_str(&info->buffer, data.str,
+					data.length - (data.length - info->specs->precision));
+			}
 		}
 		else
 		{
-			put_width_s(info, &data);
-			write_str(&info->buffer, data.str,
-					data.length - (data.length - info->specs->precision));
-		}
-	}
-	else if (info->specs->width == 0 && info->specs->precision <= data.length)
-	{
-		if (info->specs->flags->minus == 1)
-		{
-			write_str(&info->buffer, data.str,
-					data.length - (data.length - info->specs->precision));
-			put_width_s(info, &data);
-		}
-		else
-		{
-			put_width_s(info, &data);
-			write_str(&info->buffer, data.str,
-					data.length - (data.length - info->specs->precision));
-		}
-	}
-	else if (info->specs->width == 0 && info->specs->precision > data.length)
-	{
-		if (info->specs->flags->minus == 1)
-		{
-			write_str(&info->buffer, data.str, data.length);
-			put_width_s(info, &data);
-		}
-		else
-		{
-			put_width_s(info, &data);
-			write_str(&info->buffer, data.str, data.length);
+			if (info->specs->flags->minus == 1)
+			{
+				write_str(&info->buffer, data.str, data.length);
+				put_width_s(info, &data);
+			}
+			else
+			{
+				put_width_s(info, &data);
+				write_str(&info->buffer, data.str, data.length);
+			}
 		}
 	}
 	else
 	{
+		if (info->specs->flags->point == 0 || info->specs->precision == 0)
+			put_width_s(info, &data);
+		if (info->specs->precision < data.length)
+		{
+			if (info->specs->flags->minus == 1)
+			{
+				write_str(&info->buffer, data.str, data.length - (data.length - info->specs->precision));
+				put_width_s(info, &data);
+			}
+		}
+		else
+		{
+			put_width_s(info, &data);
+			write_str(&info->buffer, data.str,
+					data.length - (data.length - info->specs->precision));
+		}
 		if (info->specs->flags->minus == 1)
 		{
 			write_str(&info->buffer, data.str, data.length);
