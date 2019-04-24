22dc56fb4dfcf81d1e1fa5708e1ef32543ecf08f
4slan <amalsago@student.42.fr>
2019-04-24 19:34:05 +0200

    handeling given NULL and 0

--- sources/types/type_c.c
+++ sources/types/type_c.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 10:26:29 by amalsago          #+#    #+#             */
-/*   Updated: 2019/03/31 17:17:23 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/24 17:41:55 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,15 @@ void			type_c(const char **format, t_info *info)
 
 	info->type = **format;
 	c = (char)va_arg(info->ap, int);
-	data.str = &c;
+	if (c == NULL)
+	{
+		data.str = "";
+		info->buffer.length++;
+	}
+	else if (c == 0)
+		data.str = "";
+	else
+		data.str = &c;
 	data.length = 1;
 	data.negative = 0;
 	info->specs->flags->hash = 0;
