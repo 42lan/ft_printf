a485b19e28d831391a662e66161720e177d1c446
4slan <amalsago@student.42.fr>
2019-04-27 19:06:10 +0200

    type_p() removed

--- sources/types/types.c
+++ sources/types/types.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/21 10:54:27 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/27 16:48:32 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,6 @@ void	asterisk(const char **format, t_info *info)
 }
 
 void	type_f(const char **format, t_info *info)
-{
-	info->type = **format;
-}
-
-void	type_p(const char **format, t_info *info)
 {
 	info->type = **format;
 }
