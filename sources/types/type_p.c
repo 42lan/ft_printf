15e839ea04ff1d7b63062facc25628e401a7fd12
4slan <amalsago@student.42.fr>
2019-04-28 12:59:58 +0200

    setting to 0 values of zero, hash and plus flag

--- sources/types/type_p.c
+++ sources/types/type_p.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 16:48:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/27 19:02:52 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/28 12:01:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ void			type_p(const char **format, t_info *info)
 	info->type = **format;
 	number = (void *)va_arg(info->ap, void *);
 	data.negative = 0;
+	info->specs->flags->zero = 0;
+	info->specs->flags->hash = 0;
+	info->specs->flags->plus = 0;
 	data.str = ft_uitoa_base(number, 16, ft_isupper(info->type));
 	data.prefix = "0x";
 	data.length = ft_strlen(data.str);
