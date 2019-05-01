34643fa589e1d0955d5d838746f013add4ea568c
4slan <amalsago@student.42.fr>
2019-05-01 20:33:54 +0200

    correting part when data is 0

--- sources/types/type_c.c
+++ sources/types/type_c.c
@@ -6,11 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 10:26:29 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/25 16:08:27 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/01 20:02:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void		specs_handler(t_info *info, t_data *data, char c)
+{
+	info->specs->flags->plus = 0;
+	info->specs->flags->hash = 0;
+}
 
 void			type_c(const char **format, t_info *info)
 {
@@ -21,13 +27,14 @@ void			type_c(const char **format, t_info *info)
 	c = (char)va_arg(info->ap, int);
 	if (c == 0)
 	{
-		data.str = "\0";
-		info->buffer.length++;
+		while (info->specs->width-- > 1)
+			write_char(&info->buffer, ' ');
+		write_char(&info->buffer, c);
+		return ;
 	}
-	else
-		data.str = &c;
+	data.str = &c;
 	data.length = 1;
 	data.negative = 0;
-	info->specs->flags->hash = 0;
+	specs_handler(info, &data, c);
 	apply_specs(info, &data);
 }
