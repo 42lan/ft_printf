a427c419be7a513b7278c838df002e5b2026542b
4slan <amalsago@student.42.fr>
2019-05-01 20:37:14 +0200

    definiton of specs_handler() for type_p

--- sources/types/type_p.c
+++ sources/types/type_p.c
@@ -6,11 +6,30 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 16:48:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/28 12:01:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/04/28 16:33:13 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void		specs_handler(t_info *info, t_data *data, uintmax_t *number)
+{
+	if (info->specs->flags->hash == 1 && *number == 0
+		&& info->specs->precision != 0)
+		data->prefix = ft_strdup("");
+	else if (info->specs->flags->hash == 1)
+		data->prefix = "0";
+	if (info->specs->flags->point == 1 && info->specs->precision == 0
+		&& *number == 0)
+	{
+		if (info->specs->width == 0)
+			data->str = ft_strdup("");
+		else
+			data->str = (info->specs->flags->hash == 1)
+						? ft_strdup("0") : ft_strdup(" ");
+		data->length = 1;
+	}
+}
 
 void			type_p(const char **format, t_info *info)
 {
@@ -26,6 +45,7 @@ void			type_p(const char **format, t_info *info)
 	data.str = ft_uitoa_base(number, 16, ft_isupper(info->type));
 	data.prefix = "0x";
 	data.length = ft_strlen(data.str);
+	specs_handler(info, &data, &number);
 	apply_specs(info, &data);
 	free(data.str);
 }
