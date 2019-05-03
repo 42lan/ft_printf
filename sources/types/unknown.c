c6d3af638bad9d9cdd42975374d45266c00b641b
4slan <amalsago@student.42.fr>
2019-05-03 12:36:12 +0200

    definition of array of types to set flag space to 0 if current format char is not present in arry

--- sources/types/unknown.c
+++ sources/types/unknown.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 12:35:14 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/25 16:32:45 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/03 12:28:32 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 void			unknown(const char **format, t_info *info)
 {
 	t_data		data;
+	char		types[] = "cspdiouxXf";
 
 	info->type = **format;
 	va_arg(info->ap, int);
 	data.str = (char *)*format;
+	if (ft_strchr(types, **format) == NULL)
+		info->specs->flags->space = 0;
 	data.length = 1;
 	data.negative = 0;
 	info->specs->flags->hash = 0;
