f9739e7c7b177b3bc7a2d1e01bc194926c38fac6
4slan <amalsago@student.42.fr>
2019-05-03 12:32:29 +0200

    adding condition to exit from ft_printf() if '\n' is reached

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/22 16:44:49 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/03 12:29:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ int				ft_printf(const char *format, ...)
 		{
 			format++;
 			parsing(&format, info);
+			if (*format == 0)
+				return (0);
 		}
 		else
 			write_char(&info->buffer, *format);
