65eec34eaf8215f978eb96f17b67a59a657c27bd
4slan <amalsago@student.42.fr>
2019-05-06 17:44:12 +0200

    normalization

--- sources/types/type_p.c
+++ sources/types/type_p.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 16:48:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/06 10:39:10 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/06 17:44:04 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void			type_p(const char **format, t_info *info)
 	info->specs->flags->hash = 0;
 	info->specs->flags->plus = 0;
 	number = (void *)va_arg(info->ap, void *);
-	data.str = ft_uitoa_base_static((uintmax_t)number, 16, ft_isupper(info->type));
+	data.str = ft_uitoa_base_static((uintmax_t)number, 16,
+									ft_isupper(info->type));
 	data.length = ft_strlen(data.str);
 	data.prefix = "0x";
 	data.negative = 0;
