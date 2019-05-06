e9f2f106ea53cbca7c7c43d868f19670ccbb6c29
4slan <amalsago@student.42.fr>
2019-05-06 17:32:27 +0200

    refactoring

--- sources/types/type_c.c
+++ sources/types/type_c.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 10:26:29 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/04 16:07:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/06 10:48:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,12 @@ void			type_c(const char **format, t_info *info)
 	if (c == 0)
 	{
 		while (info->specs->width-- > 1)
-			(info->specs->flags->zero == 0)
-			? write_char(&info->buffer, ' ')
-			: write_char(&info->buffer, '0');
+			write_char(&info->buffer, (info->specs->flags->zero == 0)
+										? ' ' : '0');
 		write_char(&info->buffer, c);
 		return ;
 	}
-	data.str = (ft_isprint(c) == 1) ? &c : '\0';
+	data.str = (ft_isprint(c) == 1) ? &c : "\0";
 	data.negative = 0;
 	data.length = 1;
 	apply_specs(info, &data);
