0f1b45aa19de8bc0eaf15fb773719dcea7b950c7
4slan <amalsago@student.42.fr>
2019-05-08 16:49:00 +0200

    big char test

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:31:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/06 09:57:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/08 15:59:45 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int			main(void)
 {
-	printf("\t[ PRINTF ]\t-> Return: %d\n", printf("%d", 1));
-	printf("\t[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("%d", 1));
+	printf("\t[ PRINTF ]\t-> Return: %d\n", printf("%c", 200));
+	printf("\t[ FT_PRINTF ]\t-> Return: %d\n", ft_printf("%c", 200));
 	return (0);
 }
