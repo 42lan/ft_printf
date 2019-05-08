d8cd47005a61db96508ee2c9c913ac7b813a687c
4slan <amalsago@student.42.fr>
2019-05-08 16:46:40 +0200

    removing free_mem(); declaring structure and passing its address to initialize_info() ans so on

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,44 +6,37 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/03 12:29:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/08 15:27:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void		free_mem(t_info *info)
-{
-	free(info->specs->flags);
-	free(info->specs);
-	free(info);
-}
-
 int				ft_printf(const char *format, ...)
 {
-	t_info		*info;
+	t_info		info;
 
-	if (!format || !(info = initialize_info()))
+	if (!format)
 		return (-1);
-	va_start(info->ap, format);
+	initialize_info(&info);
+	va_start(info.ap, format);
 	while (*format)
 	{
-		if (buffer_full(&info->buffer) == 1)
-			print_buffer(&info->buffer);
+		if (buffer_full(&info.buffer) == 1)
+			print_buffer(&info.buffer);
 		if (*format == '%')
 		{
 			format++;
-			parsing(&format, info);
+			parsing(&format, &info);
 			if (*format == 0)
 				return (0);
 		}
 		else
-			write_char(&info->buffer, *format);
+			write_char(&info.buffer, *format);
 		format++;
 	}
-	if (info->buffer.index != 0)
-		print_buffer(&info->buffer);
-	va_end(info->ap);
-	free_mem(info);
-	return (info->buffer.length);
+	if (info.buffer.index != 0)
+		print_buffer(&info.buffer);
+	va_end(info.ap);
+	return (info.buffer.length);
 }
