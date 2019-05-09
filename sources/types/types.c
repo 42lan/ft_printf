befcba6615b2cef470fae6ae39a8a4899f99288f
4slan <amalsago@student.42.fr>
2019-05-09 15:25:31 +0200

    removing asterisk()

--- sources/types/types.c
+++ sources/types/types.c
@@ -6,16 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 10:20:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/27 16:48:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/09 15:13:38 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	asterisk(const char **format, t_info *info)
-{
-	info->type = **format;
-}
 
 void	type_f(const char **format, t_info *info)
 {
