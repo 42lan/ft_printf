2b2cd368c180f6db86556d6cc420ea386443ced0
4slan <amalsago@student.42.fr>
2019-05-11 19:44:33 +0200

    variable type of number changer to uintmax_t; removing ternary operator for data.length assignement

--- sources/types/type_p.c
+++ sources/types/type_p.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 16:48:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/08 18:34:46 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/11 16:56:04 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 void			type_p(const char **format, t_info *info)
 {
-	void		*number;
+	uintmax_t	number;
 	t_data		data;
 
 	info->type = **format;
 	info->hash = 0;
 	info->plus = 0;
-	number = (void *)va_arg(info->ap, void *);
-	data.str = ft_uitoa_base_static((uintmax_t)number, 16,
-									ft_isupper(info->type));
-	data.length = (number != 0) ? ft_strlen(data.str) : 1;
+	number = va_arg(info->ap, uintmax_t);
+	data.str = ft_uitoa_base_static(number, 16, ft_isupper(info->type));
+	data.length = ft_strlen(data.str);
 	data.prefix = "0x";
 	data.negative = 0;
 	apply_specs(info, &data);
