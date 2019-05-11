346bff0b54e4199ac139cad13f19f1ea4c6ffe48
4slan <amalsago@student.42.fr>
2019-05-11 19:45:23 +0200

    refactoring code

--- sources/types/type_s.c
+++ sources/types/type_s.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 14:37:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/08 17:43:59 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/11 18:42:16 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static void		write_order(t_info *info, t_data *data)
 	}
 }
 
-static void		data_null(t_info *info, t_data *data)
+static int		data_null(t_info *info, t_data *data)
 {
 	data->str = "(null)";
 	if (info->point == 1 && info->precision == 0)
@@ -36,6 +36,7 @@ static void		data_null(t_info *info, t_data *data)
 	else
 		data->length = ft_strlen(data->str);
 	write_order(info, data);
+	return (1);
 }
 
 void			type_s(const char **format, t_info *info)
@@ -45,41 +46,20 @@ void			type_s(const char **format, t_info *info)
 	info->type = **format;
 	info->hash = 0;
 	data.str = va_arg(info->ap, char *);
-	if (data.str == NULL || data.str[0] == 0)
-	{
-		if (data.str == NULL)
-			data_null(info, &data);
-		else
-		{
-			data.length = 0;
-			put_width_s(info, &data);
-		}
+	if (data.str == NULL && data_null(info, &data))
 		return ;
-	}
 	data.length = ft_strlen(data.str);
-	if (info->width <= data.length && info->point == 0)
-		write_order(info, &data);
-	else if (info->width > data.length && info->point == 0)
-		write_order(info, &data);
-	else if (info->width == 0)
+	if (info->point == 1)
 	{
-		if (info->precision <= data.length)
-			data.length -= (data.length - info->precision);
-		write_order(info, &data);
-	}
-	else
-	{
-		if (info->point == 0 || info->precision == 0)
-			put_width_s(info, &data);
-		else if (info->precision < data.length)
-		{
-			if (info->minus == 1)
-				data.length -= (data.length - info->precision);
-			else
-				data.length = info->precision;
-		}
+		if (info->precision == 0)
+			data.length = 0;
+		else if (data.str[0] == '\0')
+			data.length = data.length;
+		else if (info->precision < data.length
+				&& info->width != 0 && info->minus == 0)
+			data.length = info->precision;
 		else
 			data.length -= (data.length - info->precision);
-		write_order(info, &data);
 	}
+	write_order(info, &data);
 }
