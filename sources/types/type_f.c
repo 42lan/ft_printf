b83dd45538d5c31dd5a3a186a17902948d33282c
4slan <amalsago@student.42.fr>
2019-05-12 19:14:47 +0200

    definiton of type_f()

--- sources/types/type_f.c
+++ sources/types/type_f.c
@@ -6,13 +6,31 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/11 19:37:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/11 19:38:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/12 18:58:58 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			type_f(const char **format, t_info *info)
+static void			get_f(long double *number, t_info *info)
 {
+	if (info->length == LENGTH_LLL)
+		*number = (long double)va_arg(info->ap, long double);
+	else
+		*number = (double)va_arg(info->ap, double);
+}
+
+void				type_f(const char **format, t_info *info)
+{
+	long double		number;
+	t_data			data;
+
+	get_f(&number, info);
 	info->type = **format;
+	info->hash = 0;
+	info->plus = 0;
+	data.str = ft_ftoa_static(number, 7);
+	data.negative = 0;
+	data.length = ft_strlen(data.str);
+	apply_specs(info, &data);
 }
