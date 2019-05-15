20873582e77a36b0cb8d008966db6e307c0892ed
4slan <amalsago@student.42.fr>
2019-05-15 17:54:15 +0200

    setting precision to 0

--- sources/types/percent.c
+++ sources/types/percent.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 10:27:47 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/11 19:36:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/15 17:05:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void			percent(const char **format, t_info *info)
 
 	info->type = **format;
 	info->space = 0;
+	info->precision = 0;
 	data.str = "%";
 	data.length = 1;
 	data.negative = 0;
