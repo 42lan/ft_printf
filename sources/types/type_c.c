e729e60f0e0b6d6fe5f996a21e02fd0291b84b7c
4slan <amalsago@student.42.fr>
2019-05-15 17:54:02 +0200

    definition of write_order() for type_c()

--- sources/types/type_c.c
+++ sources/types/type_c.c
@@ -6,11 +6,35 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 10:26:29 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/15 10:51:53 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/15 16:56:52 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void		write_order(t_info *info, t_data *data)
+{
+	if (info->minus == 0)
+	{
+		if (info->zero == 0)
+			while (info->width-- > 1)
+				write_char(&info->buffer, ' ');
+		else
+			while (info->width-- > 1)
+				write_char(&info->buffer, '0');
+		write_char(&info->buffer, *data->str);
+	}
+	else
+	{
+		write_char(&info->buffer, *data->str);
+		if (info->zero == 0)
+			while (info->width-- > 1)
+				write_char(&info->buffer, ' ');
+		else
+			while (info->width-- > 1)
+				write_char(&info->buffer, '0');
+	}
+}
 
 void			type_c(const char **format, t_info *info)
 {
@@ -22,18 +46,12 @@ void			type_c(const char **format, t_info *info)
 	info->hash = 0;
 	info->space = 0;
 	c = (char)va_arg(info->ap, int);
+	data.str = &c;
 	if (c == 0)
 	{
-		if (info->zero == 0)
-			while (info->width-- > 1)
-				write_char(&info->buffer, ' ');
-		else
-			while (info->width-- > 1)
-				write_char(&info->buffer, '0');
-		write_char(&info->buffer, c);
+		write_order(info, &data);
 		return ;
 	}
-	data.str = &c;
 	data.negative = 0;
 	data.length = 1;
 	apply_specs(info, &data);
