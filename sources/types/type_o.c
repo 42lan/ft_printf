54ce3880012cacf366c88806eab54fe29b711617
4slan <amalsago@student.42.fr>
2019-05-15 17:52:00 +0200

    setting hash to 0 if nubmer is 0 and point is unsetted

--- sources/types/type_o.c
+++ sources/types/type_o.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/21 10:54:51 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/08 18:45:45 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/15 17:02:26 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,8 @@ void			type_o(const char **format, t_info *info)
 	info->type = **format;
 	get_ui(&number, info);
 	data.str = ft_uitoa_base_static(number, 8, 0);
+	if (number == 0 && info->point == 0)
+		info->hash = 0;
 	data.length = ft_strlen(data.str);
 	data.negative = 0;
 	specs_handler(info, &data, &number);
