b8f731c21c21c931a2a0f3b6e0567ae01c33f0e5
4slan <amalsago@student.42.fr>
2019-05-19 20:11:01 +0200

    specs_handler() allow to handle specification management in specific place

--- sources/types/type_f.c
+++ sources/types/type_f.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/11 19:37:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/18 16:10:41 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/19 15:44:56 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,24 @@ static void			get_f(long double *number, t_info *info)
 		*number = (double)va_arg(info->ap, double);
 }
 
+static void			specs_handler(t_info *info)
+{
+	info->hash = 0;
+	info->plus = 0;
+	if (info->point == 0)
+		info->precision = 7;
+}
+
 void				type_f(const char **format, t_info *info)
 {
 	long double		number;
 	t_data			data;
 
-	get_f(&number, info);
 	info->type = **format;
-	info->hash = 0;
-	info->plus = 0;
-	if (info->point == 0)
-		info->precision = 7;
+	get_f(&number, info);
+	specs_handler(info);
 	data.str = ft_ftoa_static(number, info->precision);
-	data.negative = 0;
 	data.length = ft_strlen(data.str);
+	data.negative = 0;
 	apply_specs(info, &data);
 }
