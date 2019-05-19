5aefae6d534622d710c6b8d98bf04e28b13ecd7c
4slan <amalsago@student.42.fr>
2019-05-19 20:10:28 +0200

    refactoring: placing data management inside type_di() because it is more logical

--- sources/types/type_di.c
+++ sources/types/type_di.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 15:03:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/15 15:11:39 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/19 17:41:30 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,16 +40,14 @@ static void		get_si(intmax_t *number, t_info *info)
 		*number = (int)va_arg(info->ap, int);
 }
 
-static void		specs_handler(t_info *info, t_data *data, intmax_t *number)
+static void		specs_handler(t_info *info, t_data *data, intmax_t number)
 {
+	if ((data->length == 1 && info->space == 0 && info->plus == 0)
+		|| info->hash == 1)
+		info->zero = 0;
 	info->hash = 0;
-	if (*number == 0 && info->point == 1 && info->precision == 0)
-		data->str[0] = (info->width != 0) ? ' ' : '\0';
-	else if (*number < 0)
-	{
-		data->negative = 1;
+	if (number < 0)
 		info->width--;
-	}
 }
 
 void			type_di(const char **format, t_info *info)
@@ -59,10 +57,14 @@ void			type_di(const char **format, t_info *info)
 
 	info->type = **format;
 	get_si(&number, info);
-	data.str = (ft_islower(info->type) && info->length == 0)
-				? ft_itoa_static(ABS(number)) : ft_litoa_static(ABS(number));
-	data.negative = 0;
+	if (number == 0 && info->point == 1 && info->precision == 0)
+		data.str = (info->width != 0) ? " " : "\0";
+	else
+		data.str = (ft_islower(info->type) && info->length == 0)
+					? ft_sitoa_base_static(ABS(number), 10, 0)
+					: ft_uitoa_base_static(ABS(number), 10, 0);
 	data.length = ft_strlen(data.str);
-	specs_handler(info, &data, &number);
+	data.negative = (number < 0) ? 1 : 0;
+	specs_handler(info, &data, number);
 	apply_specs(info, &data);
 }
