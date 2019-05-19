b7aa40941f456162919a40176f0efe73678c9d1d
4slan <amalsago@student.42.fr>
2019-05-19 20:13:34 +0200

    specs_handler() allow to handle specification management in specific place; using ternanry operator to avoid if else (-1 line)

--- sources/types/type_p.c
+++ sources/types/type_p.c
@@ -6,11 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 16:48:54 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/15 17:13:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/19 15:03:54 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void		specs_handler(t_info *info)
+{
+	info->hash = 0;
+	info->plus = 0;
+}
 
 void			type_p(const char **format, t_info *info)
 {
@@ -18,13 +24,11 @@ void			type_p(const char **format, t_info *info)
 	t_data		data;
 
 	info->type = **format;
-	info->hash = 0;
-	info->plus = 0;
+	specs_handler(info);
 	number = va_arg(info->ap, uintmax_t);
-	if (number == 0 && info->point == 1 && info->precision == 0)
-		data.str = "\0";
-	else
-		data.str = ft_uitoa_base_static(number, 16, ft_isupper(info->type));
+	data.str = (number == 0 && info->point == 1 && info->precision == 0)
+				? "\0"
+				: ft_uitoa_base_static(number, 16, ft_isupper(info->type));
 	data.length = ft_strlen(data.str);
 	data.prefix = "0x";
 	data.negative = 0;
