814f2fd84b62693a88a765935a0a965706deb711
4slan <amalsago@student.42.fr>
2019-05-20 16:38:04 +0200

    there is no need in initialize_info()

--- sources/tools/initialization.c
+++ sources/tools/initialization.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 16:22:50 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/16 12:41:23 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/20 16:21:12 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,20 @@ void	initialize_buffer(t_buffer *buffer)
 	buffer->length = 0;
 }
 
-void	initialize_info(t_info *info)
-{
-	info->type = 0;
-	initialize_specifiers(info);
-	initialize_buffer(&info->buffer);
-}
-
-void	initialize_specifiers(t_info *info)
+void	initialize_specs(t_info *info)
 {
 	info->width = 0;
-	info->precision = 0;
 	info->length = 0;
+	info->precision = 0;
 	initialize_flags(info);
 }
 
 void	initialize_flags(t_info *info)
 {
-	info->space = 0;
 	info->hash = 0;
+	info->zero = 0;
 	info->plus = 0;
 	info->minus = 0;
-	info->zero = 0;
+	info->space = 0;
 	info->point = 0;
 }
