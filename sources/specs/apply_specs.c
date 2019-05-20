43a4aa42f59f5b11c7064d31e79ffbe5dd1c5932
4slan <amalsago@student.42.fr>
2019-05-20 16:36:07 +0200

    removing useless variable width in put_width(); checking if str is NULL instead looking first char of str

--- sources/specs/apply_specs.c
+++ sources/specs/apply_specs.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 11:24:59 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/19 16:32:40 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/20 16:14:53 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ void		apply_specs(t_info *info, t_data *data)
 {
 	if (info->minus == 0)
 	{
-		if (info->zero == 1)
+		if (info->zero == 0)
 		{
-			put_prefix(info, data);
 			put_width(info, data);
+			put_prefix(info, data);
 		}
 		else
 		{
-			put_width(info, data);
 			put_prefix(info, data);
+			put_width(info, data);
 		}
 		put_precision(info, data);
 		write_str(&info->buffer, data->str, data->length);
@@ -57,28 +57,26 @@ void		put_precision(t_info *info, t_data *data)
 	int		precision;
 
 	precision = info->precision - data->length;
-	while (precision-- > 0 && data->str[0] != '\0')
+	while (precision-- > 0 && data->str != NULL)
 		write_char(&info->buffer, '0');
 }
 
 void		put_width(t_info *info, t_data *data)
 {
-	int		width;
-
 	if (info->width < data->length)
-		width = 0;
+		info->width = 0;
 	else if (info->precision > data->length)
-		width = info->width - info->precision;
+		info->width -= info->precision;
 	else if (info->hash == 1 || info->type == 'p')
-		width = info->width - data->length - ft_strlen(data->prefix);
+		info->width = info->width - data->length - ft_strlen(data->prefix);
 	else
-		width = info->width - data->length;
+		info->width -= data->length;
 	if ((info->plus == 1 || info->space == 1) && data->negative == 0)
-		width--;
+		info->width--;
 	if (info->zero == 1 && info->minus == 0 && data->length > 0)
-		while (width-- > 0)
+		while (info->width-- > 0)
 			write_char(&info->buffer, '0');
 	else
-		while (width-- > 0)
+		while (info->width-- > 0)
 			write_char(&info->buffer, ' ');
 }
