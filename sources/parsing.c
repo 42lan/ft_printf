5539a114cf97a30e0e39e5b5cab4f00752ee1e8e
4slan <amalsago@student.42.fr>
2019-05-20 16:35:16 +0200

    useless check at for 0; initialize_specifiers() renamed into initialize_specs()

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 09:25:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/16 13:37:26 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/20 15:44:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 void				parsing(const char **format, t_info *info)
 {
-	if (**format == 0)
-		return ;
-	initialize_specifiers(info);
 	info->type = 0;
+	initialize_specs(info);
 	while (**format && info->type == 0)
 		if (ft_isprint(**format))
 			g_jump_table[**format - ' '](format, info);
