854448771df43b04ec04ed895964bcfc519f2082
4slan <amalsago@student.42.fr>
2019-05-21 17:42:16 +0200

    resume of ft_printf() added

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,11 +6,22 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 05:20:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/20 15:51:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/21 17:41:50 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+/*
+** ft_printf() iterate each character of given format string until it reach '%'
+** which define a placeholder. When '%' is reached, parsing() takes over to
+** determine type of placeholder and call the appropriate function to this type.
+** On each iteration, fullness of buffer is checked:
+**		In case of fullnes, buffer is printed and its index is setted to 0
+**		to overwrite the content of buffer until it fills up.
+**		Otherwise, a new character is stored in buffer.
+** When while() loop ends, buffer is checked again to be printed.
+*/
 
 int				ft_printf(const char *format, ...)
 {
