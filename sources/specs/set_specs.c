180934c0fc9db2a0b3f5dc5804164f4f0c584a27
4slan <amalsago@student.42.fr>
2019-05-21 18:48:21 +0200

    resume of each function is added

--- sources/specs/set_specs.c
+++ sources/specs/set_specs.c
@@ -6,11 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 09:45:05 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/20 16:17:43 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/21 18:42:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+/*
+** get_number() calculates and return an integer.
+*/
 
 static int		get_number(const char **format)
 {
@@ -25,16 +29,31 @@ static int		get_number(const char **format)
 	return (number);
 }
 
+
+/*
+** digit() while parsing the formated string if previous character was not
+** a '.' it is necessarily a width, so set_width() is called.
+*/
+
 void			digit(const char **format, t_info *info)
 {
 	if (*(*format - 1) != '.')
 		set_width(format, info);
 }
 
+/*
+** set_width() calls get_number() to assign value to width specificator.
+*/
+
 void			set_width(const char **format, t_info *info)
 {
 	info->width = get_number(format);
 }
+
+/*
+** set_precision() sets flag point to 1 and calls get_number() to assign
+** value to precision specificator.
+*/
 
 void			set_precision(const char **format, t_info *info)
 {
