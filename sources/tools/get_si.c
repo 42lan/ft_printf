2125305899bbdc54bb5fdc6faecc31846aee164b
4slan <amalsago@student.42.fr>
2019-05-21 17:16:18 +0200

    using new learned skill - bitwise operation

--- sources/tools/get_si.c
+++ sources/tools/get_si.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 12:05:06 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/20 13:33:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/21 17:02:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,16 @@
 
 void	get_si(intmax_t *number, t_info *info)
 {
-	if (ft_isupper(info->type) || info->length == LENGTH_J
-		|| info->length == LENGTH_LL)
+	if (ft_isupper(info->type) || info->length & (LENGTH_J | LENGTH_LL))
 		*number = (intmax_t)va_arg(info->ap, intmax_t);
-	else if (info->length == 0 || info->length == LENGTH_T)
+	else if (info->length == 0 || info->length & LENGTH_T)
 		*number = (int)va_arg(info->ap, int);
-	else if (info->length == LENGTH_H)
+	else if (info->length & LENGTH_H)
 		*number = (short int)va_arg(info->ap, int);
-	else if (info->length == LENGTH_HH)
+	else if (info->length & LENGTH_HH)
 		*number = (signed char)va_arg(info->ap, int);
-	else if (info->length == LENGTH_L)
+	else if (info->length & LENGTH_L)
 		*number = (long int)va_arg(info->ap, long int);
-	else if (info->length == LENGTH_Z)
+	else if (info->length & LENGTH_Z)
 		*number = (size_t)va_arg(info->ap, size_t);
 }
