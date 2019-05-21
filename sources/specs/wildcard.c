59ec8d83bd84f596c386408206e619c778ee699e
4slan <amalsago@student.42.fr>
2019-05-21 18:48:39 +0200

    resume of wildcard() is added

--- sources/specs/wildcard.c
+++ sources/specs/wildcard.c
@@ -6,11 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 15:14:26 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/11 13:37:19 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/21 18:46:29 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+/*
+** wildcard() handle asterisk '*' so that width and/or precision fields might
+** be omitted, or a numeric integer value, or a dynamic value when passed as
+** another argument.
+*/
 
 void			wildcard(const char **format, t_info *info)
 {
