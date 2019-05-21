866d6b2655cc26481e64e4a69c68cc87efc5ff97
4slan <amalsago@student.42.fr>
2019-05-21 18:47:55 +0200

    general resume of functions is added

--- sources/specs/set_length.c
+++ sources/specs/set_length.c
@@ -6,11 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 17:04:35 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/19 16:15:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/21 18:37:36 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+/*
+** Below are the functions that sets corresponding length of a placeholder.
+*/
 
 void	length_h(const char **format, t_info *info)
 {
