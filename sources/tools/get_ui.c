05c52fb7211056553389fcd693e187d6831b1ea7
4slan <amalsago@student.42.fr>
2019-05-21 17:16:34 +0200

    using new learned skill - bitwise operation

--- sources/tools/get_ui.c
+++ sources/tools/get_ui.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 12:01:08 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/20 13:33:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/05/21 17:01:29 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@
 
 void	get_ui(uintmax_t *number, t_info *info)
 {
-	if ((info->length == 0 || info->length == LENGTH_T)
+	if ((info->length == 0 || info->length & LENGTH_T)
 		&& (info->type != 'O' && info->type != 'U'))
 		*number = (unsigned int)va_arg(info->ap, unsigned int);
 	else if ((ft_isupper(info->type) && info->type != 'X')
-			|| info->length == LENGTH_LL || info->length == LENGTH_J)
+			|| info->length & (LENGTH_LL | LENGTH_J))
 		*number = (uintmax_t)va_arg(info->ap, uintmax_t);
-	else if (info->length == LENGTH_H)
+	else if (info->length & LENGTH_H)
 		*number = (unsigned short int)va_arg(info->ap, int);
-	else if (info->length == LENGTH_HH)
+	else if (info->length & LENGTH_HH)
 		*number = (unsigned char)va_arg(info->ap, int);
-	else if (info->length == LENGTH_L)
+	else if (info->length & LENGTH_L)
 		*number = (unsigned long int)va_arg(info->ap, unsigned long int);
-	else if (info->length == LENGTH_Z)
+	else if (info->length & LENGTH_Z)
 		*number = (size_t)va_arg(info->ap, size_t);
 }
